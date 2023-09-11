0e32db6682bc709909cc93f8b92e465434e10700
Viggsengetass <paulito.antoine@gmail.com>
2023-09-11 16:39:27 +0200

    fix_config_connection

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'mfc');
-define('DB_PASSWORD', 'monstrefightingclub');
+define('DB_PASSWORD', 'monstre-f-club');
 define('DB_NAME', 'mfc');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
