140f1e5cdb1a65e7a3c36c5bd55aadfd3aacf540
Viggsengetass <paulito.antoine@gmail.com>
2023-09-11 16:47:21 +0200

    init_index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil</title>
+    <?php include 'common.php'; ?>
+    <link rel="stylesheet" href="/style/index.css">
+    <script src="/js/index.js"></script>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+<div id="global">
+    <p id="anim" aria-label="CodePen">
+        <span data-text="M">M</span>
+        <span data-text="O">O</span>
+        <span data-text="N">N</span>
+        <span data-text="S">S</span>
+        <span data-text="T">T</span>
+        <span data-text="R">R</span>
+        <span data-text="E">E</span>
+        <span data-text="-">-</span>
+        <span data-text="F">F</span>
+        <span data-text=".">.</span>
+        <span data-text="C">C</span>
+        <span data-text=".">.</span>
+    </p>
+    <div id="word-wraper">
+        <div class="word"></div>
+    </div>
+    <div id="btn-rank">
+        <a href="ranking.php" class="btn btn-primary btn-lg">Tuer nathael</a>
+    </div>
+</div>
+</body>
+</html>
