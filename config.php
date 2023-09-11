a132aeda2285e9ac78b439b5e38f6fe0c13438b2
Viggsengetass <paulito.antoine@gmail.com>
2023-09-11 16:27:56 +0200

    init_config

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'mfc');
+define('DB_PASSWORD', 'monstrefightingclub');
+define('DB_NAME', 'mfc');
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//if (!$conn) {
+//    die("ERREUR : Impossible de se connecter à la base de données. " . mysqli_connect_error());
+//}
+// elseif ($conn) {
+//    echo "Connexion réussie";
+//} else {
+//    echo "jsp moi";
+//}
+?>
