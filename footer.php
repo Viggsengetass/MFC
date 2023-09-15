70861b62b7d972aa84e41d176bad78e98eea5e41
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-09-15 11:41:20 +0200

    init_footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="bg-gray-900 text-white py-8">
+    <!-- Pied de page, informations de contact, liens vers les réseaux sociaux, etc. -->
+    <div class="container mx-auto flex justify-between items-center">
+        <div>
+            <p class="text-xl font-semibold">Contactez-nous : contact@mfc-mma.com</p>
+            <ul class="flex space-x-4 mt-4">
+                <li><a href="#" class="text-white hover:text-blue-500"><img src="facebook.png" alt="Facebook"></a></li>
+                <li><a href="#" class="text-white hover:text-blue-500"><img src="twitter.png" alt="Twitter"></a></li>
+                <li><a href="#" class="text-white hover:text-blue-500"><img src="instagram.png" alt="Instagram"></a></li>
+            </ul>
+        </div>
+        <div>
+            <!-- Ajoutez des animations et des effets ici si nécessaire -->
+        </div>
+    </div>
+</footer>
+</body>
+</html>
