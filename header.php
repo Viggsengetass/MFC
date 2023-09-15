0521a5b769a4bfdbece00db33e6907624e2e71c3
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-09-15 11:40:21 +0200

    init_header

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - M.F.C MMA Tournament</title>
+    <link rel="stylesheet" href="common.css"> <!-- Inclusion de la feuille de style CSS commune -->
+    <link rel="stylesheet" href="tailwind.min.css"> <!-- Inclusion de Tailwind CSS -->
+    <link rel="stylesheet" href="header.css"> <!-- Fichier de surcharge CSS pour l'en-tête -->
+</head>
+<body>
+<header class="bg-blue-700 text-white py-4">
+    <!-- Entête du site, logo, menu de navigation, etc. -->
+    <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-2xl font-bold">Bienvenue au M.F.C MMA Tournament</h1>
+        <nav>
+            <ul class="flex space-x-4">
+                <li><a href="#" class="text-white hover:underline">Accueil</a></li>
+                <li><a href="#" class="text-white hover:underline">Événements</a></li>
+                <li><a href="#" class="text-white hover:underline">Billets</a></li>
+                <li><a href="#" class="text-white hover:underline">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
