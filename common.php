7067dc136b0b3c7241dcd1b12a8f8178ae0dcb6f
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-10-25 14:13:01 +0200

    integer_tailwind

--- common.php
+++ common.php
@@ -1,6 +1,6 @@
 <link rel="icon" href="/assets/img/remarques.png" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<!--<link rel="stylesheet" href="/style/header.css">-->
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <link rel="stylesheet" href="/style/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
