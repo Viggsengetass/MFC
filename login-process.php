9c75f76bb0b95fa67ccb962cb918003d9137c215
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-10-25 16:47:15 +0200

    add_login

---
+++ login-process.php
@@ -0,0 +1,68 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Utilisez la fonction de traitement pour vérifier la connexion
+    $success = loginUser($username, $password);
+
+    if ($success) {
+        // Rediriger vers la page d'accueil après la connexion réussie
+        header('Location: index.php');
+    } else {
+        // Rediriger vers la page de connexion avec un message d'erreur
+        header('Location: login.php?error=1');
+    }
+}
+
+// Fonction pour vérifier la connexion d'un utilisateur
+function loginUser($username, $password) {
+    global $conn;
+
+    // Recherchez l'utilisateur par nom d'utilisateur dans la base de données
+    $query = "SELECT id, nom, mot_de_passe FROM users WHERE nom = ?";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        // La préparation de la requête a échoué
+        return false;
+    }
+
+    // Liez le paramètre
+    $bindResult = $stmt->bind_param("s", $username);
+
+    if (!$bindResult) {
+        // La liaison des paramètres a échoué
+        return false;
+    }
+
+    // Exécutez la requête
+    $executeResult = $stmt->execute();
+
+    if (!$executeResult) {
+        // L'exécution de la requête a échoué
+        return false;
+    }
+
+    // Récupérez le résultat de la requête
+    $stmt->store_result();
+    $stmt->bind_result($userId, $username, $hashedPassword);
+
+    if ($stmt->num_rows == 1) {
+        // L'utilisateur existe
+        $stmt->fetch();
+
+        // Vérifiez le mot de passe
+        if (password_verify($password, $hashedPassword)) {
+            // Le mot de passe est correct
+            return true;
+        }
+    }
+
+    return false; // La connexion a échoué
+}
--- login.php
+++ login.php
@@ -1,65 +1,47 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - M.F.C MMA Tournament</title>
-    <link rel="stylesheet" href="/style/login.css">
     <?php include 'common.php'; ?>
-
+    <link rel="stylesheet" href="/style/register.css">
 </head>
 <body class="bg-gray-100">
-<?php include 'header.php'; ?> <!-- Inclure le header -->
-<div class="min-h-screen flex items-center justify-center">
-    <div class="max-w-md w-full bg-white p-8 rounded shadow-lg">
-        <h1 class="text-3xl font-semibold mb-6">Connexion</h1>
-        <form method="post" action="login-process.php">
-            <div class="mb-4">
-                <label for="username" class="block text-gray-600">Nom d'utilisateur</label>
-                <input type="text" id="username" name="username" class="form-input" required>
-            </div>
-            <div class="mb-6">
-                <label for="password" class="block text-gray-600">Mot de passe</label>
-                <input type="password" id="password" name="password" class="form-input" required>
-            </div>
-            <div class="flex justify-end">
-                <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Se connecter</button>
-            </div>
-        </form>
-    </div>
+<div class="container">
+    <input type="checkbox" id="login_toggle">
+    <form class="form" method="post" action="login-process.php" class="space-y-4">
+        <div class="form_front">
+            <div class="form_details">Login</div>
+            <input placeholder="Username" class="input" type="text">
+            <input placeholder="Password" class="input" type="password">
+            <button class="btn">Login</button>
+            <span class="switch">Don't have an account?
+                <label class="login_tog" for="login_toggle">
+                    Sign Up
+                </label>
+            </span>
+        </div>
+        <div class="form_back">
+            <div class="form_details">Sign Up</div>
+            <input placeholder="Firstname" class="input" type="text">
+            <input placeholder="Username" class="input" type="text">
+            <input placeholder="Email" class="input" type="text">
+            <input placeholder="Password" class="input" type="password">
+            <input placeholder="Confirm Password" class="input" type="password">
+            <button class="btn">Signup</button>
+            <span class="switch">Already have an account?
+                <label class="login_tog" for="login_toggle">
+                    Sign In
+                </label>
+            </span>
+        </div>
+    </form>
 </div>
 </body>
 </html>
-
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include 'config.php'; // Inclure le fichier de configuration de la base de données
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Utilisez la fonction de traitement pour vérifier les informations de connexion
-    if (verifyLogin($username, $password)) {
-        // Rediriger vers la page appropriée (par exemple, l'espace client)
-        header('Location: client-dashboard.php');
-    } else {
-        // Rediriger vers la page de connexion avec un message d'erreur
-        $_SESSION['login_error'] = 'Identifiants incorrects';
-        header('Location: login.php');
-    }
-}
-
-// Fonction pour vérifier les informations de connexion
-function verifyLogin($username, $password) {
-    global $conn;
-    $query = "SELECT * FROM users WHERE username = ? AND password = ?";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    return ($result->num_rows == 1);
-}
-?>
