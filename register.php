7385081711a03819adcb01f482f59086e2c3e2df
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-10-25 14:30:30 +0200

    correct_register

--- register.php
+++ register.php
@@ -23,7 +23,7 @@
             </div>
             <div>
                 <label for="email" class="block text-gray-600">Email</label>
-                <input type="email" id="email" name "email" class="form-input" required>
+                <input type="email" id="email" name="email" class="form-input" required>
             </div>
             <div>
                 <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
@@ -35,32 +35,3 @@
 </div>
 </body>
 </html>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include 'config.php'; // Inclure le fichier de configuration de la base de données
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-
-    // Utilisez la fonction de traitement pour effectuer l'inscription
-    if (registerUser($username, $password, $email)) {
-        // Rediriger vers la page de connexion après l'inscription réussie
-        header('Location: login.php');
-    } else {
-        // Rediriger vers la page d'inscription avec un message d'erreur
-        header('Location: register.php?error=1');
-    }
-}
-
-// Fonction pour effectuer l'inscription d'un utilisateur
-function registerUser($username, $password, $email) {
-    global $conn;
-    $query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("sss", $username, $password, $email);
-
-    return $stmt->execute();
-}
-?>
