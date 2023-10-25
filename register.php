09378bb1a75ef8c317def996ea5fb474e687842f
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-10-25 16:12:36 +0200

    new_form

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -12,30 +13,37 @@ error_reporting(E_ALL);
     <link rel="stylesheet" href="/style/register.css">
 </head>
 <body class="bg-gray-100">
-<?php include 'header.php'; ?> <!-- Inclure le header -->
-<div class="min-h-screen flex items-center justify-center">
-    <div class="max-w-md w-full bg-white p-8 rounded shadow-lg">
-        <h1 class="text-3xl font-semibold text-center mb-6">Inscription</h1>
-        <form method="post" action="register-process.php" class="space-y-4">
-            <div>
-                <label for="username" class="block text-gray-600">Nom d'utilisateur</label>
-                <input type="text" id="username" name="username" class="form-input" required>
-            </div>
-            <div>
-                <label for="password" class="block text-gray-600">Mot de passe</label>
-                <input type="password" id="password" name="password" class="form-input" required>
-            </div>
-            <div>
-                <label for="email" class "block text-gray-600">Email</label>
-                <input type="email" id="email" name="email" class="form-input" required>
-            </div>
-            <div>
-                <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                    S'inscrire
-                </button>
-            </div>
-        </form>
-    </div>
+<div class="container">
+    <input type="checkbox" id="signup_toggle">
+    <form class="form">
+        <div class="form_front">
+            <div class="form_details">Login</div>
+            <input placeholder="Username" class="input" type="text">
+            <input placeholder="Password" class="input" type="text">
+            <input placeholder="Email" class="input" type="text">
+
+            <button class="btn">Login</button>
+            <span class="switch">Don't have an account?
+                <label class="signup_tog" for="signup_toggle">
+                    Sign Up
+                </label>
+            </span>
+        </div>
+        <div class="form_back">
+            <div class="form_details">SignUp</div>
+            <input placeholder="Firstname" class="input" type="text">
+            <input placeholder="Username" class="input" type="text">
+            <input placeholder="Email" class="input" type="text">
+            <input placeholder="Password" class="input" type="text">
+            <input placeholder="Confirm Password" class="input" type="text">
+            <button class="btn">Signup</button>
+            <span class="switch">Already have an account?
+                <label class="signup_tog" for="signup_toggle">
+                    Sign In
+                </label>
+            </span>
+        </div>
+    </form>
 </div>
 </body>
 </html>
