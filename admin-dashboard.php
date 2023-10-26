fbe15a00c152a5672b3f8c1139c62f39bd444a86
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-10-26 15:10:36 +0200

    init_admin_dashboard

---
+++ admin-dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de bord administrateur - M.F.C MMA Tournament</title>
+    <?php include 'common.php'; ?>
+    <link rel="stylesheet" href="/style/admin-dashboard.css">
+</head>
+<body>
+
+<!-- En-tête -->
+<?php include 'header.php'; ?>
+
+<!-- Menu de navigation -->
+<?php include 'admin-sidebar.php'; ?>
+
+<!-- Contenu du tableau de bord administrateur -->
+<div id="content">
+    <h1>Tableau de bord administrateur</h1>
+    <!-- Ajoutez ici les différentes fonctionnalités pour gérer les combattants, les catégories, les événements, etc. -->
+</div>
+
+<!-- Pied de page -->
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
---
+++ admin-sidebar.php
@@ -0,0 +1,9 @@
+<div id="sidebar">
+    <ul>
+        <li><a href="admin-dashboard.php">Tableau de bord</a></li>
+        <li><a href="admin-manage-combattants.php">Gérer les Combattants</a></li>
+        <li><a href="admin-create-event.php">Créer un Événement</a></li>
+        <li><a href="admin-list-events.php">Liste des Événements</a></li>
+        <li><a href="logout.php">Déconnexion</a></li>
+    </ul>
+</div>
