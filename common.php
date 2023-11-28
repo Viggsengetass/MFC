1dbffe4af02ab86858b90475f1cd08c1d6de9b74
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-11-28 08:55:52 +0100

    Revert "url_tailwind"
    This reverts commit fc37c5500c81798562ceca67835f258b332bc0c5.

--- common.php
+++ common.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" href="/style/common.css">
 <link rel="stylesheet" href="/general/output.css">
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
