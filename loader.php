c6a6d1a798e6e3b5d2d688135100afb803ca45c5
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-11-28 15:56:13 +0100

    init_loader

---
+++ loader.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="/style/loader.css">
+<div class="pl">
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__dot"></div>
+    <div class="pl__text">Loading…</div>
+</div>
+</html>
