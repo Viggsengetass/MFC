7d3c9a2a9b5450eb74c76907b2c0dfdd39f6bbbf
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-11-29 10:15:13 +0100

    check_login

--- login.php
+++ login.php
@@ -16,41 +16,53 @@ session_start(); // Démarrez la session
 </head>
 <body class="bg-gray-100">
 <div class="container">
-    <input type="checkbox" id="login_toggle">
-    <form class="form" method="post" action="login-process.php" class="space-y-4">
-        <div class="form_front">
-            <div class="form_details">Login</div>
-            <?php
-            // Affichez un message d'erreur s'il y en a un
-            if (isset($_SESSION['login_error'])) {
-                echo '<p class="error">' . $_SESSION['login_error'] . '</p>';
-                unset($_SESSION['login_error']); // Nettoyez la variable de session après l'affichage
-            }
-            ?>
-            <input placeholder="Username" name="username" class="input" type="text">
-            <input placeholder="Password" name="password" class="input" type="password">
-            <button class="btn" type="submit">Login</button>
-            <span class="switch">Don't have an account?
-                <label class="login_tog" for="login_toggle">
-                    Sign Up
-                </label>
-            </span>
-        </div>
-        <div class="form_back">
-            <div class="form_details">Sign Up</div>
-            <input placeholder="Firstname" class="input" type="text">
-            <input placeholder="Username" class="input" type="text">
-            <input placeholder="Email" class="input" type="text">
-            <input placeholder="Password" class="input" type="password">
-            <input placeholder="Confirm Password" class="input" type="password">
-            <button class="btn">Signup</button>
-            <span class="switch">Already have an account?
-                <label class="login_tog" for="login_toggle">
-                    Sign In
-                </label>
-            </span>
-        </div>
-    </form>
+    <?php
+    // Vérifiez si l'utilisateur est connecté
+    if (isset($_SESSION['user'])) {
+        echo '<p>Bienvenue, ' . $_SESSION['user']['username'] . '!</p>';
+        echo '<p>Vous êtes connecté en tant que ' . $_SESSION['user']['role'] . '.</p>';
+        echo '<p><a href="logout.php">Déconnexion</a></p>';
+    } else {
+        // L'utilisateur n'est pas connecté, affichez le formulaire de connexion
+        ?>
+        <input type="checkbox" id="login_toggle">
+        <form class="form" method="post" action="login-process.php" class="space-y-4">
+            <div class="form_front">
+                <div class="form_details">Login</div>
+                <?php
+                // Affichez un message d'erreur s'il y en a un
+                if (isset($_SESSION['login_error'])) {
+                    echo '<p class="error">' . $_SESSION['login_error'] . '</p>';
+                    unset($_SESSION['login_error']); // Nettoyez la variable de session après l'affichage
+                }
+                ?>
+                <input placeholder="Username" name="username" class="input" type="text">
+                <input placeholder="Password" name="password" class="input" type="password">
+                <button class="btn" type="submit">Login</button>
+                <span class="switch">Don't have an account?
+                    <label class="login_tog" for="login_toggle">
+                        Sign Up
+                    </label>
+                </span>
+            </div>
+            <div class="form_back">
+                <div class="form_details">Sign Up</div>
+                <input placeholder="Firstname" class="input" type="text">
+                <input placeholder="Username" class="input" type="text">
+                <input placeholder="Email" class="input" type="text">
+                <input placeholder="Password" class="input" type="password">
+                <input placeholder="Confirm Password" class="input" type="password">
+                <button class="btn">Signup</button>
+                <span class="switch">Already have an account?
+                    <label class="login_tog" for="login_toggle">
+                        Sign In
+                    </label>
+                </span>
+            </div>
+        </form>
+        <?php
+    }
+    ?>
 </div>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset(); // Supprime toutes les données de session
 session_destroy(); // Détruit la session
 
-// Redirige vers la page de connexion ou toute autre page souhaitée
+// Redirige vers la page de connexion
 header('Location: login.php');
 exit();
 ?>
