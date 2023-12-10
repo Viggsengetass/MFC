6321087efc3217aad70efd65d9b00d1ce0a56b02
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 23:07:27 +0100

    fix_event

--- admin-functions.php
+++ admin-functions.php
@@ -3,32 +3,6 @@
 
 include 'config.php';
 
-// Fonction pour récupérer le nom de la catégorie
-function getCategoryName($category_id, $conn) {
-    $query = "SELECT name FROM categories WHERE id = ?";
-    $stmt = $conn->prepare($query);
-
-    if (!$stmt) {
-        die("Erreur de préparation de la requête: " . $conn->error);
-    }
-
-    $stmt->bind_param("i", $category_id);
-
-    if (!$stmt->execute()) {
-        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
-    }
-
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row['name'];
-    } else {
-        return 'Inconnue';
-    }
-}
-
-// Fonction pour créer un combattant
 function createCombattant($nom, $prenom, $surnom, $description, $image, $categorie_id, $conn) {
     $query = "INSERT INTO combattants_admin (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
@@ -47,7 +21,6 @@ function createCombattant($nom, $prenom, $surnom, $description, $image, $categor
     return true;
 }
 
-// Fonction pour récupérer tous les combattants
 function getAllCombattants($conn) {
     $query = "SELECT * FROM combattants_admin";
     $result = $conn->query($query);
@@ -67,8 +40,65 @@ function getAllCombattants($conn) {
     return $combattants;
 }
 
-// Fonction pour valider un combattant
 function validateCombatant($nom, $prenom, $description, $image, $categorie_id) {
     return !empty($nom) && !empty($prenom) && !empty($description) && !empty($image) && $categorie_id !== false;
+}
+
+function getCategoryName($id, $conn) {
+    $query = "SELECT name FROM categories WHERE id = ?";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        die("Erreur de préparation de la requête: " . $conn->error);
+    }
+
+    $stmt->bind_param("i", $id);
+
+    if (!$stmt->execute()) {
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
+    }
+
+    $stmt->bind_result($name);
+    $stmt->fetch();
+    $stmt->close();
+
+    return $name;
+}
+
+function createEvenement($nom, $date, $lieu, $description, $conn) {
+    $query = "INSERT INTO evenements_admin (nom, date, lieu, description) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        die("Erreur de préparation de la requête: " . $conn->error);
+    }
+
+    $stmt->bind_param("ssss", $nom, $date, $lieu, $description);
+
+    if (!$stmt->execute()) {
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
+    }
+
+    $stmt->close();
+    return true;
+}
+
+function getAllEvenements($conn) {
+    $query = "SELECT * FROM evenements_admin";
+    $result = $conn->query($query);
+
+    if (!$result) {
+        die("Erreur lors de l'exécution de la requête: " . $conn->error);
+    }
+
+    $evenements = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $evenements[] = $row;
+        }
+    }
+
+    return $evenements;
 }
 ?>
--- evenements.php
+++ evenements.php
@@ -11,21 +11,6 @@ include 'admin-functions.php';
 
 // Récupère tous les événements
 $evenements = getAllEvenements($conn);
-
-// Nombre d'événements par page
-$evenementsParPage = 6;
-
-// Nombre total de pages
-$nombrePages = ceil(count($evenements) / $evenementsParPage);
-
-// Page actuelle, par défaut 1
-$pageActuelle = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
-
-// Indice de début pour la pagination
-$indiceDebut = ($pageActuelle - 1) * $evenementsParPage;
-
-// Événements à afficher sur la page actuelle
-$evenementsPageActuelle = array_slice($evenements, $indiceDebut, $evenementsParPage);
 ?>
 
 <!DOCTYPE html>
@@ -53,24 +38,14 @@ $evenementsPageActuelle = array_slice($evenements, $indiceDebut, $evenementsParP
 <div id="content" class="container mx-auto mt-8">
     <h1 class="text-3xl font-bold w-full mb-4">Liste des Événements</h1>
 
-    <?php foreach ($evenementsPageActuelle as $evenement) : ?>
+    <?php foreach ($evenements as $evenement) : ?>
         <div class="carte-evenement p-4 rounded-md mb-4">
-            <img src="<?= $evenement['image'] ?>" alt="<?= $evenement['nom'] ?>">
             <h2 class="text-xl font-bold"><?= $evenement['nom'] ?></h2>
             <p class="mt-2"><strong>Date:</strong> <?= $evenement['date'] ?></p>
             <p class="mt-2"><strong>Lieu:</strong> <?= $evenement['lieu'] ?></p>
             <p class="mt-2"><strong>Description:</strong> <?= $evenement['description'] ?></p>
         </div>
     <?php endforeach; ?>
-
-    <!-- Afficher la pagination si nécessaire -->
-    <?php if ($nombrePages > 1) : ?>
-        <div class="flex justify-center mt-4">
-            <?php for ($i = 1; $i <= $nombrePages; $i++) : ?>
-                <a href="?page=<?= $i ?>" class="mx-2 p-2 bg-gray-800 text-white rounded-full"><?= $i ?></a>
-            <?php endfor; ?>
-        </div>
-    <?php endif; ?>
 </div>
 </body>
 </html>
