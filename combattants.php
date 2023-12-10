17ca53197fe7ab0bc47ebbeebd8df6726ed6402c
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 21:50:44 +0100

    fix_combattants

--- combattants.php
+++ combattants.php
@@ -1,7 +1,9 @@
 <?php
+// combattants.php
+
 // Active l'affichage des erreurs pendant le développement
 error_reporting(E_ALL);
-ini_set('display_errors', 1;
+ini_set('display_errors', 1);
 
 // Inclut les fichiers nécessaires
 include 'common.php';
@@ -18,7 +20,6 @@ $combattants = getAllCombattants($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
     <link rel="stylesheet" href="/style/dark-neumorphic.css">
-    <!-- Pas besoin de re-déclarer le lien Tailwind ici car il est déjà inclus dans common.php -->
 </head>
 <body>
 <div id="content" class="container mx-auto mt-8 flex flex-wrap">
@@ -26,9 +27,9 @@ $combattants = getAllCombattants($conn);
 
     <?php foreach ($combattants as $combattant) : ?>
         <div class="carte-combattant p-4 rounded-md mb-4">
-            <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>" class="rounded-full mb-2" style="width: 80px; height: 80px;">
-            <h2 class="text-xl font-semibold mb-2"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-            <p><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
+            <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
+            <h2 class="text-xl font-bold"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
+            <p class="mt-2"><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
             <p class="mt-2"><strong>Description:</strong> <?= $combattant['description'] ?></p>
             <p class="mt-2"><strong>Catégorie:</strong> <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></p>
         </div>
