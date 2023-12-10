0d1a7b93a3fe2ae90aa798ee65b951b17eb44a63
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 15:50:52 +0100

    fix_logout

--- logout.php
+++ logout.php
@@ -1,7 +1,14 @@
 <?php
-session_start();
-session_unset(); // Supprime toutes les données de session
-session_destroy(); // Détruit la session
+// Assurez-vous que la session est démarrée
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Supprime toutes les données de session
+session_unset();
+
+// Détruit la session
+session_destroy();
 
 // Redirige vers la page de connexion
 header('Location: login.php');
