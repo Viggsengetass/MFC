f4e36cf98bd8c9b8ba5bfcc67af299ec4f5cf723
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 21:55:40 +0100

    combattants_style

--- combattants.php
+++ combattants.php
@@ -20,19 +20,38 @@ $combattants = getAllCombattants($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
     <link rel="stylesheet" href="/style/dark-neumorphic.css">
+    <style>
+        .carte-combattant {
+            width: 200px; /* Réduire la largeur des cartes */
+            margin: 8px; /* Ajouter de l'espace entre les cartes */
+        }
+
+        /* Créer une grille pour afficher 3 cartes par ligne */
+        #content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        }
+    </style>
 </head>
 <body>
-<div id="content" class="container mx-auto mt-8 flex flex-wrap">
+<div id="content" class="container mx-auto mt-8">
     <h1 class="text-3xl font-bold w-full mb-4">Liste des Combattants</h1>
 
+    <?php $count = 0; ?>
     <?php foreach ($combattants as $combattant) : ?>
-        <div class="carte-combattant p-4 rounded-md mb-4">
-            <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
-            <h2 class="text-xl font-bold"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-            <p class="mt-2"><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
-            <p class="mt-2"><strong>Description:</strong> <?= $combattant['description'] ?></p>
-            <p class="mt-2"><strong>Catégorie:</strong> <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></p>
-        </div>
+    <div class="carte-combattant p-4 rounded-md mb-4">
+        <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
+        <h2 class="text-xl font-bold"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
+        <p class="mt-2"><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
+        <p class="mt-2"><strong>Description:</strong> <?= $combattant['description'] ?></p>
+        <p class="mt-2"><strong>Catégorie:</strong> <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></p>
+    </div>
+    <?php $count++; ?>
+    <?php if ($count % 6 === 0) : ?> <!-- Début d'un nouveau groupe de 6 cartes -->
+</div>
+<div class="container mx-auto mt-8">
+    <?php endif; ?>
     <?php endforeach; ?>
 </div>
 </body>
