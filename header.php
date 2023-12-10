154bef4a03223ddd47d69fbd492e839178c53926
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 19:02:49 +0100

    Revert "fix_header"
    This reverts commit 303b6e28f02e09ad25cac1912f2dab2426b789fb.

--- header.php
+++ header.php
@@ -1,54 +1,49 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Déplacez la session_start() en haut du fichier, avant tout affichage ou inclusion
 session_start();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion - M.F.C MMA Tournament</title>
-    <?php include 'common.php'; ?>
-    <link rel="stylesheet" href="/style/register.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta charset="utf-8" />
+    <meta name="Description" content="entertainment and Art" />
+    <title>Accueil - M.F.C MMA Tournament</title>
+    <link rel="stylesheet" href="/style/header.css">
+    <link rel="stylesheet" href="https://unpkg.com/feather-icons/dist/feather.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 
-<body class="bg-gray-100">
-<div class="container">
-    <?php
-    if (isset($_SESSION['user'])) {
-        echo '<p>Bienvenue, ' . $_SESSION['user']['username'] . '!</p>';
-        echo '<p>Vous êtes connecté en tant que ' . $_SESSION['user']['role'] . '.</p>';
-        echo '<p><a href="logout.php">Déconnexion</a></p>';
-    } else {
-        ?>
-        <input type="checkbox" id="login_toggle">
-        <form class="form" method="post" action="login-process.php" class="space-y-4">
+<body>
+<nav>
+    <div class="menu">
+        <input type="checkbox" id="check">
+        <div class="logo"><a href="#"><img src="/img/logo.png" alt="MFC Logo" class="logo-img"></a></div>
+        <ul>
+            <label class="btn cancel" for="check"><i class="fas fa-times"></i></label>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="combattants.php">Combattants</a></li>
+            <li><a href="evenements.php">Événements</a></li>
+            <li><a href="contact.php">Contact</a></li>
+
             <?php
-            if (isset($_SESSION['login_error'])) {
-                echo '<p class="error">' . $_SESSION['login_error'] . '</p>';
-                unset($_SESSION['login_error']);
+            if (isset($_SESSION['user'])) {
+                echo '<li>Bienvenue, ' . $_SESSION['user']['username'] . '!</li>';
+                echo '<li><a href="logout.php">Déconnexion</a></li>';
+            } else {
+                echo '<li><a href="login.php">Se Connecter</a></li>';
             }
             ?>
-            <div class="form_front">
-                <div class="form_details">Login</div>
-                <input placeholder="Username" name="username" class="input" type="text">
-                <input placeholder="Password" name="password" class="input" type="password">
-                <button class="btn" type="submit">Login</button>
-                <span class="switch">Don't have an account?
-                        <a href="register.php">Sign Up</a>
-                    </span>
-            </div>
-        </form>
-        <?php
-    }
-    ?>
-</div>
+        </ul>
+        <label for="check" class="btn bars"><i class="fas fa-bars"></i></label>
+    </div>
+</nav>
+<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<script>
+    feather.replace();
+</script>
 </body>
 
 </html>
