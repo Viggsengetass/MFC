65084b2cad42ef493fa6f5a91f12fbc000bbd8b0
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 22:18:51 +0100

    Revert "combattants_style"
    This reverts commit f8bba1360d38669dfead5e700259dd9443b92a32.

--- combattants.php
+++ combattants.php
@@ -11,6 +11,21 @@ include 'admin-functions.php';
 
 // Récupère tous les combattants
 $combattants = getAllCombattants($conn);
+
+// Nombre de combattants par page
+$combattantsParPage = 6;
+
+// Nombre total de pages
+$nombrePages = ceil(count($combattants) / $combattantsParPage);
+
+// Page actuelle, par défaut 1
+$pageActuelle = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+
+// Indice de début pour la pagination
+$indiceDebut = ($pageActuelle - 1) * $combattantsParPage;
+
+// Combattants à afficher sur la page actuelle
+$combattantsPageActuelle = array_slice($combattants, $indiceDebut, $combattantsParPage);
 ?>
 
 <!DOCTYPE html>
@@ -20,21 +35,42 @@ $combattants = getAllCombattants($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
     <link rel="stylesheet" href="/style/dark-neumorphic.css">
-    <link rel="stylesheet" href="/style/combattants.css">
+    <style>
+        .carte-combattant {
+            width: 200px; /* Réduire la largeur des cartes */
+            margin: 8px; /* Ajouter de l'espace entre les cartes */
+        }
+
+        /* Créer une grille pour afficher 3 cartes par ligne */
+        #content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        }
+    </style>
 </head>
 <body>
-<div id="content">
-    <h1>Liste des Combattants</h1>
+<div id="content" class="container mx-auto mt-8">
+    <h1 class="text-3xl font-bold w-full mb-4">Liste des Combattants</h1>
 
-    <?php foreach ($combattants as $combattant) : ?>
-        <div class="carte-combattant">
+    <?php foreach ($combattantsPageActuelle as $combattant) : ?>
+        <div class="carte-combattant p-4 rounded-md mb-4">
             <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
-            <h2><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-            <p><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
-            <p><strong>Description:</strong> <?= $combattant['description'] ?></p>
-            <p><strong>Catégorie:</strong> <?= getCategoryName($combattant['categorie_id'], $conn) ?></p>
+            <h2 class="text-xl font-bold"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
+            <p class="mt-2"><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
+            <p class="mt-2"><strong>Description:</strong> <?= $combattant['description'] ?></p>
+            <p class="mt-2"><strong>Catégorie:</strong> <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></p>
         </div>
     <?php endforeach; ?>
+
+    <!-- Afficher la pagination si nécessaire -->
+    <?php if ($nombrePages > 1) : ?>
+        <div class="flex justify-center mt-4">
+            <?php for ($i = 1; $i <= $nombrePages; $i++) : ?>
+                <a href="?page=<?= $i ?>" class="mx-2 p-2 bg-gray-800 text-white rounded-full"><?= $i ?></a>
+            <?php endfor; ?>
+        </div>
+    <?php endif; ?>
 </div>
 </body>
 </html>
