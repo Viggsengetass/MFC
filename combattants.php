ac72131a93bf9b35dbea95000c38d60bd914981c
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-10 22:11:00 +0100

    style_combattants

--- combattants.php
+++ combattants.php
@@ -1,6 +1,6 @@
-<?php
-// combattants.php
+<!-- combattants.php -->
 
+<?php
 // Active l'affichage des erreurs pendant le développement
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -9,8 +9,14 @@ ini_set('display_errors', 1);
 include 'common.php';
 include 'admin-functions.php';
 
-// Récupère tous les combattants
-$combattants = getAllCombattants($conn);
+// Définir le nombre de cartes par page
+$cartesParPage = 4;
+
+// Récupère le numéro de la page actuelle
+$pageActuelle = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+// Récupère tous les combattants pour la page actuelle
+$combattants = getCombattantsParPage($conn, $pageActuelle, $cartesParPage);
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +25,6 @@ $combattants = getAllCombattants($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
-<!--    <link rel="stylesheet" href="/style/dark-neumorphic.css">-->
     <link rel="stylesheet" href="/style/combattants.css">
 </head>
 <body>
@@ -35,6 +40,16 @@ $combattants = getAllCombattants($conn);
             <p><strong>Catégorie:</strong> <?= getCategoryName($combattant['categorie_id'], $conn) ?></p>
         </div>
     <?php endforeach; ?>
+
+    <!-- Pagination -->
+    <div class="pagination">
+        <?php
+        $totalPages = getTotalPages($conn, $cartesParPage);
+        for ($i = 1; $i <= $totalPages; $i++) :
+            ?>
+            <a href="?page=<?= $i ?>" class="<?= ($i == $pageActuelle) ? 'active' : '' ?>"><?= $i ?></a>
+        <?php endfor; ?>
+    </div>
 </div>
 </body>
 </html>
