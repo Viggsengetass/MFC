5813a75b125409a24025e1c757cd00518eb553e7
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-11 16:41:43 +0100

    new

--- combattants.php
+++ combattants.php
@@ -1,4 +1,21 @@
-<!-- combattants.php -->
+<?php
+// combattants.php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include 'common.php';
+include 'admin-functions.php';
+
+$combattants = getAllCombattants($conn);
+
+$combattantsParPage = 6;
+$nombrePages = ceil(count($combattants) / $combattantsParPage);
+$pageActuelle = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+$indiceDebut = ($pageActuelle - 1) * $combattantsParPage;
+$combattantsPageActuelle = array_slice($combattants, $indiceDebut, $combattantsParPage);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,28 +23,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
     <link rel="stylesheet" href="/style/carousel.css">
+    <script src="/js/carousel.js"></script>
+
 </head>
 <body>
 
 <div class="container">
-    <div class="carousel">
-        <?php foreach ($combattants as $combattant) : ?>
-            <div class="card">
-                <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
-                <div class="content">
-                    <h2><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-                    <span>Surnom: <?= $combattant['surnom'] ?></span>
-                    <span>Description: <?= $combattant['description'] ?></span>
-                    <span>Catégorie: <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></span>
-                </div>
+    <?php foreach ($combattants as $combattant) : ?>
+        <div class="card">
+            <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
+            <div class="content">
+                <h2><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
+                <span>Surnom: <?= $combattant['surnom'] ?></span>
+                <span>Description: <?= $combattant['description'] ?></span>
+                <span>Catégorie: <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></span>
             </div>
-        <?php endforeach; ?>
-    </div>
-    <div class="arrow arrow-left">&lt;</div>
-    <div class="arrow arrow-right">&gt;</div>
+        </div>
+    <?php endforeach; ?>
 </div>
-
-<script src="/scripts/carousel.js"></script>
 
 </body>
 </html>
