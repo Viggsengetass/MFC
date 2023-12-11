42cad8eee97b0a39f6cbf5d31ad103201fee3958
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-11 11:37:38 +0100

    Revert "fix_index"
    This reverts commit 1f834c2e7c01a00413bfa577788b1da5d64a99a7.

--- index.php
+++ index.php
@@ -16,21 +16,17 @@
     <h2 class="text-3xl font-semibold">Événements à venir</h2>
     <!-- Liste des événements à venir -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        <?php if (isset($evenements) && is_array($evenements)) : ?>
-            <?php foreach ($evenements as $evenement) : ?>
-                <div class="bg-white p-4 rounded-lg shadow">
-                    <?php if (!empty($evenement['image1']) && !empty($evenement['combattant1_nom'])) : ?>
-                        <img src="<?= $evenement['image1'] ?>" alt="<?= $evenement['combattant1_nom'] ?>" class="w-full h-40 object-cover rounded">
-                    <?php endif; ?>
-                    <h3 class="text-xl font-semibold mt-2"><?= $evenement['nom'] ?></h3>
-                    <p class="text-gray-500">Date : <?= date('d-m-Y', strtotime($evenement['date'])) ?></p>
-                    <p class="mt-2"><?= $evenement['description'] ?></p>
-                    <a href="#" class="text-blue-600 hover:underline mt-2">Réserver des billets</a>
-                </div>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <p>Aucun événement à venir pour le moment.</p>
-        <?php endif; ?>
+        <?php foreach ($evenements as $evenement) : ?>
+            <div class="bg-white p-4 rounded-lg shadow">
+                <?php if (!empty($evenement['image1']) && !empty($evenement['combattant1_nom'])) : ?>
+                    <img src="<?= $evenement['image1'] ?>" alt="<?= $evenement['combattant1_nom'] ?>" class="w-full h-40 object-cover rounded">
+                <?php endif; ?>
+                <h3 class="text-xl font-semibold mt-2"><?= $evenement['nom'] ?></h3>
+                <p class="text-gray-500">Date : <?= date('d-m-Y', strtotime($evenement['date'])) ?></p>
+                <p class="mt-2"><?= $evenement['description'] ?></p>
+                <a href="#" class="text-blue-600 hover:underline mt-2">Réserver des billets</a>
+            </div>
+        <?php endforeach; ?>
     </div>
 </section>
 
