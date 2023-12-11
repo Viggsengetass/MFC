33501e12ca9112c544cf9b3a22d8fb7fd6b5259b
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-11 13:59:24 +0100

    update

--- combattants.php
+++ combattants.php
@@ -23,58 +23,82 @@ $combattantsPageActuelle = array_slice($combattants, $indiceDebut, $combattantsP
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Combattants</title>
     <link rel="stylesheet" href="/style/dark-neumorphic.css">
-    <link rel="stylesheet" href="/style/carousel.css">
     <style>
-        #content {
-            display: none;
+        body {
+            min-height: 100vh;
+            display: grid;
+            place-items: center;
+            background: #100721;
+            font-family: "Ubuntu Mono", monospace;
+            font-weight: 400;
+            margin: 0;
         }
 
-        .carousel-item .content {
-            display: none;
+        .container {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            height: 500px;
+            gap: 10px;
+            margin-top: 20%;
+        }
+
+        .container > div {
+            flex: 0 0 250px;
+            border-radius: 0.5rem;
+            transition: 0.5s ease-in-out;
+            cursor: pointer;
+            box-shadow: 1px 5px 15px #1e0e3e;
+            position: relative;
+            overflow: hidden;
+        }
+
+        .container > div .content {
+            font-size: 1.5rem;
+            color: #fff;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            height: 100%;
+            justify-content: flex-end;
+            background: #02022e;
+            background: linear-gradient(0deg, rgba(2, 2, 46, 0.6755077031) 0%, rgba(255, 255, 255, 0) 100%);
+            transition: transform 0.5s ease-in-out;
+            transform: translateY(100%);
+        }
+
+        .container > div .content span {
+            display: block;
+            margin-top: 5px;
+            font-size: 1.2rem;
+        }
+
+        .container > div:hover {
+            flex: 0 0 250px;
+            box-shadow: 1px 3px 15px #7645d8;
+            transform: translateY(-30px);
+        }
+
+        .container > div:hover .content {
+            transform: translateY(0%);
         }
     </style>
 </head>
-<body style="margin-top: 20%;">
+<body>
 
-<div id="content" class="container mx-auto mt-8">
-    <h1 class="text-3xl font-bold w-full mb-4">Liste des Combattants</h1>
-    <?php foreach ($combattantsPageActuelle as $combattant) : ?>
-        <div class="carte-combattant p-4 rounded-md mb-4">
+<div class="container">
+    <?php foreach ($combattants as $combattant) : ?>
+        <div>
             <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
-            <h2 class="text-xl font-bold"><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-            <p class="mt-2"><strong>Surnom:</strong> <?= $combattant['surnom'] ?></p>
-            <p class="mt-2"><strong>Description:</strong> <?= $combattant['description'] ?></p>
-            <p class="mt-2"><strong>Catégorie:</strong> <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></p>
+            <div class="content">
+                <h2><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
+                <span>Surnom: <?= $combattant['surnom'] ?></span>
+                <span>Description: <?= $combattant['description'] ?></span>
+                <span>Catégorie: <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></span>
+            </div>
         </div>
     <?php endforeach; ?>
 </div>
-
-<div class="carousel-container">
-    <div class="carousel-wrapper">
-        <?php foreach ($combattants as $combattant) : ?>
-            <div class="carousel-item">
-                <img src="<?= $combattant['image'] ?>" alt="<?= $combattant['nom'] . ' ' . $combattant['prenom'] ?>">
-                <div class="content">
-                    <h2><?= $combattant['prenom'] . ' ' . $combattant['nom'] ?></h2>
-                    <span>Surnom: <?= $combattant['surnom'] ?></span>
-                    <span>Description: <?= $combattant['description'] ?></span>
-                    <span>Catégorie: <?= isset($categories[$combattant['categorie_id']]) ? $categories[$combattant['categorie_id']] : 'Inconnue' ?></span>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</div>
-
-<script>
-    const carouselWrapper = document.querySelector('.carousel-wrapper');
-    let currentIndex = 0;
-
-    setInterval(() => {
-        currentIndex = (currentIndex + 1) % <?= count($combattants) ?>;
-        const translateValue = -currentIndex * 260;
-        carouselWrapper.style.transform = `translateX(${translateValue}px)`;
-    }, 3000);
-</script>
 
 </body>
 </html>
