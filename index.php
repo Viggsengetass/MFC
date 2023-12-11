5737254d4d4d5dd025238a63b5b4d3b73456db60
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-11 09:04:47 +0100

    fix_index

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
+// index.php
+
+// Active l'affichage des erreurs pendant le développement
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Inclut les fichiers nécessaires
 include 'common.php';
 include 'admin-functions.php';
 
-// Récupère tous les événements
+// Récupère tous les événements triés par date
 $evenements = getAllEvenements($conn);
+usort($evenements, function($a, $b) {
+    return strtotime($a['date']) - strtotime($b['date']);
+});
+
 ?>
 
 <!DOCTYPE html>
@@ -15,19 +25,6 @@ $evenements = getAllEvenements($conn);
     <title>Accueil - M.F.C MMA Tournament</title>
     <?php include 'common.php'; ?>
     <link rel="stylesheet" href="/style/index.css">
-    <style>
-        body {
-            background-color: #1a1a1a;
-            color: #fff;
-            margin-top: 20%;
-        }
-
-        section {
-            color: #000; /* Couleur du texte dans la section (vous pouvez ajuster si nécessaire) */
-        }
-
-        /* Ajoutez d'autres styles ici selon vos besoins */
-    </style>
     <script src="/js/index.js"></script>
 </head>
 <body>
@@ -37,11 +34,7 @@ $evenements = getAllEvenements($conn);
     <h2 class="text-3xl font-semibold">Événements à venir</h2>
     <!-- Liste des événements à venir -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-
-        <?php
-        // Parcourir les événements et les afficher dans les cartes
-        foreach ($evenements as $evenement) :
-            ?>
+        <?php foreach ($evenements as $evenement) : ?>
             <div class="bg-white p-4 rounded-lg shadow">
                 <img src="<?= $evenement['image1'] ?>" alt="<?= $evenement['combattant1_nom'] ?>" class="w-full h-40 object-cover rounded">
                 <h3 class="text-xl font-semibold mt-2"><?= $evenement['nom'] ?></h3>
@@ -50,7 +43,6 @@ $evenements = getAllEvenements($conn);
                 <a href="#" class="text-blue-600 hover:underline mt-2">Réserver des billets</a>
             </div>
         <?php endforeach; ?>
-
     </div>
 </section>
 
