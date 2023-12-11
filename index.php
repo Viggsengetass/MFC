a48c86188c1dde9f28085eea6f32f3c1045fe7a0
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-11 08:51:22 +0100

    add_index

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
     <?php include 'common.php'; ?>
     <link rel="stylesheet" href="/style/index.css">
     <script src="/js/index.js"></script>
-    <style>
-        /* Ajoutez des styles personnalisés si nécessaire */
-    </style>
 </head>
 <body>
 
@@ -18,20 +15,23 @@
     <h2 class="text-3xl font-semibold">Événements à venir</h2>
     <!-- Liste des événements à venir -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+
         <?php
-        // Récupère tous les événements
+        // Récupérer tous les événements
         $evenements = getAllEvenements($conn);
 
-        // Affiche les cartes d'événements dynamiquement
-        foreach ($evenements as $evenement) : ?>
+        // Parcourir les événements et les afficher dans les cartes
+        foreach ($evenements as $evenement) :
+            ?>
             <div class="bg-white p-4 rounded-lg shadow">
                 <img src="<?= $evenement['image1'] ?>" alt="<?= $evenement['combattant1_nom'] ?>" class="w-full h-40 object-cover rounded">
                 <h3 class="text-xl font-semibold mt-2"><?= $evenement['nom'] ?></h3>
-                <p class="text-gray-500">Date : <?= date('d/m/Y', strtotime($evenement['date'])) ?></p>
+                <p class="text-gray-500">Date : <?= date('d-m-Y', strtotime($evenement['date'])) ?></p>
                 <p class="mt-2"><?= $evenement['description'] ?></p>
                 <a href="#" class="text-blue-600 hover:underline mt-2">Réserver des billets</a>
             </div>
         <?php endforeach; ?>
+
     </div>
 </section>
 
