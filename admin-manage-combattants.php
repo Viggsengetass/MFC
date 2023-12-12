e0619845781429a4dd7b62b7865f13b53177c87a
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-13 00:01:25 +0100

    style_admin-manage-combattant

--- admin-manage-combattants.php
+++ admin-manage-combattants.php
@@ -26,37 +26,64 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
         // Add Tailwind CSS configuration here if needed
     </script>
 </head>
-<body class="bg-gray-100">
+<body class="bg-gray-900 text-gray-100">
 <div id="sidebar">
     <!-- Include the sidebar here -->
 </div>
-<div id="content" class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Gérer les Combattants</h1>
+<div id="content" class="p-4 mt-10">
+    <h1 class="text-3xl font-bold mb-6">Gérer les Combattants</h1>
 
-    <button id="addCombatantBtn" class="mb-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600">Ajouter un combattant</button>
+    <button id="addCombatantBtn" class="mb-4 p-2 bg-blue-600 text-white rounded hover:bg-blue-700">Ajouter un combattant</button>
 
-    <div id="addCombatantForm" style="display:none;" class="mb-8">
-        <form method="POST" enctype="multipart/form-data">
-            <input type="text" name="nom" placeholder="Nom" required class="mb-2 p-2 border border-gray-300 rounded">
-            <input type="text" name="prenom" placeholder="Prénom" required class="mb-2 p-2 border border-gray-300 rounded">
-            <input type="text" name="surnom" placeholder="Surnom" class="mb-2 p-2 border border-gray-300 rounded">
-            <textarea name="description" placeholder="Description" class="mb-2 p-2 border border-gray-300 rounded"></textarea>
-            <input type="file" name="image" class="mb-2">
-            <input type="number" name="categorie_id" placeholder="Catégorie ID" required class="mb-2 p-2 border border-gray-300 rounded">
-            <input type="submit" name="add_combattant" value="Créer le combattant" class="p-2 bg-green-500 text-white rounded hover:bg-green-600">
+    <div id="addCombatantForm" style="display:none;" class="mb-8 p-6 bg-gray-800 rounded">
+        <form method="POST" enctype="multipart/form-data" class="space-y-4">
+            <div>
+                <label for="nom" class="block mb-2">Nom:</label>
+                <input type="text" name="nom" id="nom" placeholder="Nom" required class="w-full p-2 border border-gray-700 bg-gray-700 rounded">
+            </div>
+
+            <div>
+                <label for="prenom" class="block mb-2">Prénom:</label>
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom" required class="w-full p-2 border border-gray-700 bg-gray-700 rounded">
+            </div>
+
+            <div>
+                <label for="surnom" class="block mb-2">Surnom:</label>
+                <input type="text" name="surnom" id="surnom" placeholder="Surnom" class="w-full p-2 border border-gray-700 bg-gray-700 rounded">
+            </div>
+
+            <div>
+                <label for="description" class="block mb-2">Description:</label>
+                <textarea name="description" id="description" placeholder="Description" class="w-full p-2 border border-gray-700 bg-gray-700 rounded"></textarea>
+            </div>
+
+            <div>
+                <label for="image" class="block mb-2">Image:</label>
+                <input type="file" name="image" id="image" class="w-full p-2 border border-gray-700 bg-gray-700 rounded">
+            </div>
+
+            <div>
+                <label for="categorie_id" class="block mb-2">Catégorie ID:</label>
+                <input type="number" name="categorie_id" id="categorie_id" placeholder="Catégorie ID" required class="w-full p-2 border border-gray-700 bg-gray-700 rounded">
+            </div>
+
+            <div>
+                <input type="submit" name="add_combattant" value="Créer le combattant" class="p-2 w-full bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer">
+            </div>
         </form>
     </div>
 
     <!-- Liste des combattants -->
     <div class="grid grid-cols-3 gap-4">
         <?php foreach ($combattants as $combattant): ?>
-            <div class="bg-white p-4 rounded shadow">
-                <img class="rounded" src="<?= htmlspecialchars($combattant['image']) ?>" alt="Image" style="width: 100px; height: 100px; object-fit: cover;">
-                <h2 class="text-xl"><?= htmlspecialchars($combattant['nom']) ?></h2>
+            <div class="bg-gray-800 p-4 rounded shadow">
+                <img class="rounded mb-4" src="<?= htmlspecialchars($combattant['image']) ?>" alt="Image" style="width: 100%; height: 200px; object-fit: cover;">
+                <h2 class="text-xl mb-2"><?= htmlspecialchars($combattant['nom']) ?></h2>
+                <p class="mb-4"><?= htmlspecialchars($combattant['description']) ?></p>
                 <!-- Autres détails du combattant -->
-                <div class="mt-4">
-                    <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="text-blue-500 hover:underline">Éditer</a>
-                    <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="text-red-500 hover:underline">Supprimer</a>
+                <div class="flex justify-between">
+                    <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="text-blue-400 hover:underline">Éditer</a>
+                    <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="text-red-400 hover:underline">Supprimer</a>
                 </div>
             </div>
         <?php endforeach; ?>
