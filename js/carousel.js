7ec35a1f0098dd2aa74b5819993741af90f94310
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-12 16:06:19 +0100

    ddd

--- js/carousel.js
+++ js/carousel.js
@@ -1,50 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const carousel = document.querySelector('.carousel');
+    const container = document.querySelector('.container');
     const cards = document.querySelectorAll('.card');
-    const arrowLeft = document.querySelector('.arrow-left');
-    const arrowRight = document.querySelector('.arrow-right');
+    let expandedCard = null;
 
-    let currentIndex = 0;
-    let intervalId;
-
-    function showCard(index) {
-        cards.forEach((card, i) => {
-            card.classList.remove('expanded');
-            if (i === index) {
-                card.classList.add('expanded');
-            }
-        });
-    }
-
-    arrowLeft.addEventListener('click', function () {
-        currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-        showCard(currentIndex);
-    });
-
-    arrowRight.addEventListener('click', function () {
-        currentIndex = (currentIndex + 1) % cards.length;
-        showCard(currentIndex);
-    });
-
-    function autoScroll() {
-        currentIndex = (currentIndex + 1) % cards.length;
-        showCard(currentIndex);
-    }
-
-    // Défilement automatique toutes les 4 secondes
-    intervalId = setInterval(autoScroll, 4000);
-
-    // Arrête le défilement automatique au survol d'une carte
     cards.forEach((card) => {
         card.addEventListener('mouseenter', function () {
-            clearInterval(intervalId);
+            if (expandedCard !== null) {
+                expandedCard.classList.remove('expanded');
+            }
+            card.classList.add('expanded');
+            expandedCard = card;
         });
 
         card.addEventListener('mouseleave', function () {
-            intervalId = setInterval(autoScroll, 4000);
+            card.classList.remove('expanded');
+            expandedCard = null;
         });
     });
-
-    // Afficher la première carte
-    showCard(currentIndex);
 });
