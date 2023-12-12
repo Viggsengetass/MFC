b289815a1b00b5ce730340f057064a63c705425b
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-12 16:14:55 +0100

    ccc

--- js/carousel.js
+++ js/carousel.js
@@ -12,4 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
             card.classList.remove('expanded');
         });
     });
+
+    const arrowLeft = document.querySelector('.arrow-left');
+    const arrowRight = document.querySelector('.arrow-right');
+    let currentIndex = 0;
+
+    arrowLeft.addEventListener('click', function () {
+        currentIndex = Math.max(currentIndex - 1, 0);
+        showCard(currentIndex);
+    });
+
+    arrowRight.addEventListener('click', function () {
+        currentIndex = Math.min(currentIndex + 1, cards.length - 1);
+        showCard(currentIndex);
+    });
+
+    function showCard(index) {
+        container.style.transform = `translateX(-${index * (200 + 10)}px)`;
+    }
 });
