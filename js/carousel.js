0e4f51d22e2d6478cd6b5d43d0235184c530c117
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-12 15:58:31 +0100

    dddddddd

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
     const cards = document.querySelectorAll('.card');
+    const arrowLeft = document.querySelector('.arrow-left');
+    const arrowRight = document.querySelector('.arrow-right');
 
-    cards.forEach((card, index) => {
-        card.addEventListener('mouseover', function () {
-            // Réinitialise toutes les cartes pour qu'elles ne soient pas étendues
-            cards.forEach((c) => c.classList.remove('expanded'));
-            card.classList.add('expanded');
-        });
+    let currentIndex = 0;
+    let intervalId;
 
-        card.addEventListener('mouseout', function () {
+    function showCard(index) {
+        cards.forEach((card, i) => {
             card.classList.remove('expanded');
+            if (i === index) {
+                card.classList.add('expanded');
+            }
         });
+        // Défilement automatique toutes les 4 secondes
+        clearInterval(intervalId);
+        intervalId = setInterval(autoScroll, 4000);
+    }
+
+    arrowLeft.addEventListener('click', function () {
+        currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+        showCard(currentIndex);
     });
+
+    arrowRight.addEventListener('click', function () {
+        currentIndex = (currentIndex + 1) % cards.length;
+        showCard(currentIndex);
+    });
+
+    function autoScroll() {
+        currentIndex = (currentIndex + 1) % cards.length;
+        showCard(currentIndex);
+    }
+
+    // Afficher la première carte
+    showCard(currentIndex);
 });
