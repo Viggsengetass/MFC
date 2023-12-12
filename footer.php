24f11d4b5c0243fb6e5cf2cf3bfef7fd6953fdd5
TomVicTO <Tomvictor.pro@gmail.com>
2023-12-12 16:51:26 +0100

    modif footer + contact

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 <main>
 </main>
 
-<footer class="rounded-lg shadow m-4 bg-gray-800 text-white" style="background-color: #333033;">
+<footer class="rounded-lg shadow m-4 bg-gray-800 text-white zindex" style="background-color: #333033;">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">MFC</a>. All Rights Reserved.</span>
         <ul class="flex flex-wrap items-center mt-3 space-x-4 items-center text-sm font-medium text-gray-500">
