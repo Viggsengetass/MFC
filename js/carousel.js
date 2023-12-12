4dba5a636b977ec6be8116825315abec2d3827bf
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-12 16:08:18 +0100

    ddd

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.querySelector('.container');
     const cards = document.querySelectorAll('.card');
-    let expandedCard = null;
 
     cards.forEach((card) => {
         card.addEventListener('mouseenter', function () {
-            if (expandedCard !== null) {
-                expandedCard.classList.remove('expanded');
-            }
+            cards.forEach((c) => c.classList.remove('expanded'));
             card.classList.add('expanded');
-            expandedCard = card;
-        });
-
-        card.addEventListener('mouseleave', function () {
-            card.classList.remove('expanded');
-            expandedCard = null;
         });
     });
 });
