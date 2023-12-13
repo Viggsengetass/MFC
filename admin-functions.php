db4cff2c5ed583ac20347c7c79be8c55d1e870f9
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-13 09:15:14 +0100

    Revert "fix_admin-functions"
    This reverts commit 2095fbec2b1ff080c31c631565571f980d160240.

--- admin-functions.php
+++ admin-functions.php
@@ -1,33 +1,37 @@
 <?php
-// Inclure le fichier commun pour la connexion à la base de données
-include 'common.php';
+// admin-functions.php
+
+include 'common.php'; // Utilisez 'common.php' pour les fonctions communes
 
 // Créer un nouveau combattant
 function createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
     if (empty($nom) || empty($prenom) || empty($description) || empty($image) || empty($categorie_id)) {
-        return false; // Validation de base des champs
+        return false; // Les champs obligatoires sont vides
     }
 
-    $query = "INSERT INTO combattants_admin (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)";
+    $query = "INSERT INTO combattants (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
+
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
 
     $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id);
+
     if (!$stmt->execute()) {
         $stmt->close();
-        return false; // Échec de l'exécution
+        return false; // Échec de l'exécution de la requête
     }
 
     $stmt->close();
-    return true; // Succès
+    return true; // Création réussie
 }
 
-// Obtenir tous les combattants
+// Récupérer tous les combattants
 function getAllCombattants($conn) {
-    $query = "SELECT * FROM combattants_admin";
+    $query = "SELECT * FROM combattants";
     $result = $conn->query($query);
+
     if (!$result) {
         die("Erreur lors de l'exécution de la requête: " . $conn->error);
     }
@@ -39,15 +43,17 @@ function getAllCombattants($conn) {
     return $combattants;
 }
 
-// Mettre à jour un combattant
+// Mettre à jour les informations d'un combattant
 function updateCombattant($conn, $id, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
-    $query = "UPDATE combattants_admin SET nom = ?, prenom = ?, surnom = ?, description = ?, image = ?, categorie_id = ? WHERE id = ?";
+    $query = "UPDATE combattants SET nom = ?, prenom = ?, surnom = ?, description = ?, image = ?, categorie_id = ? WHERE id = ?";
     $stmt = $conn->prepare($query);
+
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
 
-    $stmt->bind_param("sssssii", $nom, $prenom, $surnom, $description, $image, $categorie_id, $id);
+    $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id, $id);
+
     if (!$stmt->execute()) {
         $stmt->close();
         return false;
@@ -59,13 +65,15 @@ function updateCombattant($conn, $id, $nom, $prenom, $surnom, $description, $ima
 
 // Supprimer un combattant
 function deleteCombattant($conn, $id) {
-    $query = "DELETE FROM combattants_admin WHERE id = ?";
+    $query = "DELETE FROM combattants WHERE id = ?";
     $stmt = $conn->prepare($query);
+
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
 
     $stmt->bind_param("i", $id);
+
     if (!$stmt->execute()) {
         $stmt->close();
         return false;
@@ -75,24 +83,28 @@ function deleteCombattant($conn, $id) {
     return true;
 }
 
-// Obtenir les détails d'un combattant spécifique
+// Récupérer les détails d'un combattant spécifique
 function getCombattant($conn, $id) {
-    $query = "SELECT * FROM combattants_admin WHERE id = ?";
+    $query = "SELECT * FROM combattants WHERE id = ?";
     $stmt = $conn->prepare($query);
+
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
 
     $stmt->bind_param("i", $id);
+
     if (!$stmt->execute()) {
         die("Erreur lors de l'exécution de la requête: " . $stmt->error);
     }
 
     $result = $stmt->get_result();
+
     if ($result->num_rows === 1) {
         return $result->fetch_assoc();
     } else {
         return null; // Aucun combattant trouvé avec cet ID
     }
 }
+
 ?>
