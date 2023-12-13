8a727c9233213bd29ee4f77b2680d021de7f9768
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-13 09:37:33 +0100

    Revert "fix_admin-functions"
    This reverts commit ed6a287171f26546f680f0665cf0a25575547589.

--- admin-functions.php
+++ admin-functions.php
@@ -1,97 +1,154 @@
 <?php
-include 'common.php'; // Assurez-vous que ce fichier contient les informations de connexion à la base de données
+// admin-functions.php
 
-// Créer un nouveau combattant
-function createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
-    if (empty($nom) || empty($prenom) || empty($description) || empty($image) || empty($categorie_id)) {
-        return false; // Validation des champs
-    }
+include 'config.php';
 
+function createCombattant($nom, $prenom, $surnom, $description, $image, $categorie_id, $conn) {
     $query = "INSERT INTO combattants_admin (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
+
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
 
     $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id);
+
     if (!$stmt->execute()) {
-        $stmt->close();
-        return false;
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
     }
 
     $stmt->close();
     return true;
 }
 
-// Récupérer tous les combattants
 function getAllCombattants($conn) {
     $query = "SELECT * FROM combattants_admin";
     $result = $conn->query($query);
+
     if (!$result) {
         die("Erreur lors de l'exécution de la requête: " . $conn->error);
     }
 
     $combattants = [];
-    while ($row = $result->fetch_assoc()) {
-        $combattants[] = $row;
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $combattants[] = $row;
+        }
     }
+
     return $combattants;
 }
 
-// Mettre à jour les informations d'un combattant
+function validateCombatant($nom, $prenom, $description, $image, $categorie_id) {
+    return !empty($nom) && !empty($prenom) && !empty($description) && !empty($image) && $categorie_id !== false;
+}
+
+function getCategoryName($id, $conn) {
+    $query = "SELECT name FROM categories WHERE id = ?";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        die("Erreur de préparation de la requête: " . $conn->error);
+    }
+
+    $stmt->bind_param("i", $id);
+
+    if (!$stmt->execute()) {
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
+    }
+
+    $stmt->bind_result($name);
+    $stmt->fetch();
+    $stmt->close();
+
+    return $name;
+}
+
+function createEvenement($nom, $date, $lieu, $description, $conn) {
+    $query = "INSERT INTO evenements_admin (nom, date, lieu, description) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($query);
+
+    if (!$stmt) {
+        die("Erreur de préparation de la requête: " . $conn->error);
+    }
+
+    $stmt->bind_param("ssss", $nom, $date, $lieu, $description);
+
+    if (!$stmt->execute()) {
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
+    }
+
+    $stmt->close();
+    return true;
+}
+
+function getAllEvenements($conn) {
+    $query = "SELECT * FROM evenements_admin ORDER BY date ASC";
+    $result = $conn->query($query);
+
+    if (!$result) {
+        die("Erreur lors de l'exécution de la requête: " . $conn->error);
+    }
+
+    $evenements = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $evenements[] = $row;
+        }
+    }
+
+    return $evenements;
+}
+
+// Update combatant information
 function updateCombattant($conn, $id, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
     $query = "UPDATE combattants_admin SET nom = ?, prenom = ?, surnom = ?, description = ?, image = ?, categorie_id = ? WHERE id = ?";
     $stmt = $conn->prepare($query);
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
-
     $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id, $id);
     if (!$stmt->execute()) {
-        $stmt->close();
-        return false;
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
     }
-
     $stmt->close();
     return true;
 }
 
-// Supprimer un combattant
+// Delete a combatant from the database
 function deleteCombattant($conn, $id) {
     $query = "DELETE FROM combattants_admin WHERE id = ?";
     $stmt = $conn->prepare($query);
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
-
     $stmt->bind_param("i", $id);
     if (!$stmt->execute()) {
-        $stmt->close();
-        return false;
+        die("Erreur lors de l'exécution de la requête: " . $stmt->error);
     }
-
     $stmt->close();
     return true;
 }
 
-// Récupérer les détails d'un combattant spécifique
+// Function to retrieve a single combatant's details
 function getCombattant($conn, $id) {
     $query = "SELECT * FROM combattants_admin WHERE id = ?";
     $stmt = $conn->prepare($query);
     if (!$stmt) {
         die("Erreur de préparation de la requête: " . $conn->error);
     }
-
     $stmt->bind_param("i", $id);
     if (!$stmt->execute()) {
         die("Erreur lors de l'exécution de la requête: " . $stmt->error);
     }
-
     $result = $stmt->get_result();
     if ($result->num_rows === 1) {
         return $result->fetch_assoc();
     } else {
-        return null; // Aucun combattant trouvé avec cet ID
+        return null; // No combatant found with this ID
     }
 }
+
 ?>
