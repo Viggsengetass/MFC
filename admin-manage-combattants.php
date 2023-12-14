0fca3d2d9b54733446b7ef40c5888a153e2309ed
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 22:20:18 +0100

    ddd

--- admin-manage-combattants.php
+++ admin-manage-combattants.php
@@ -6,37 +6,45 @@ error_reporting(E_ALL);
 require_once 'admin-functions.php';
 require_once 'common.php';
 
+// Vérifiez l'accès administrateur
+// checkAdmin();
+
+// Récupération de tous les combattants
 $combattants = getAllCombattants($conn);
 
+// Fonction de validation du combattant
+function validateCombattant($nom, $prenom, $surnom, $description, $categorie_id, $image) {
+    // Effectuez vos validations ici
+    // ...
+    return true;
+}
+
+// Fonction pour créer un combattant dans la base de données
+function createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
+    // Traitez l'upload de l'image et insérez les données dans la base de données
+    // ...
+    return true;
+}
+
+// Traitement du formulaire d'ajout d'un combattant
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $surnom = $_POST['surnom'];
     $description = $_POST['description'];
     $categorie_id = $_POST['categorie_id'];
+    $image = $_FILES['image'];
 
-    $image = null;
-    $targetDirectory = "/var/www/vhosts/nice-meitner.164-90-190-187.plesk.page/httpdocs/image-combattants/";
-
-    if (!file_exists($targetDirectory)) {
-        if (!mkdir($targetDirectory, 0755, true)) {
-            die('Échec de la création des répertoires...');
-        }
-    }
-
-    if (!empty($_FILES['image']['name'])) {
-        $targetFile = $targetDirectory . basename($_FILES['image']['name']);
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $targetFile)) {
-            $image = "image-combattants/" . basename($_FILES['image']['name']);
+    // Validation des données
+    if (validateCombattant($nom, $prenom, $surnom, $description, $categorie_id, $image)) {
+        if (createCombattant($conn, $nom, $prenom, $surnom, $description, $image['name'], $categorie_id)) {
+            // Redirection ou affichage d'un message de succès
+            echo "Combattant ajouté avec succès.";
         } else {
-            echo "<p>Erreur lors du téléchargement de l'image.</p>";
+            echo "Erreur lors de l'ajout du combattant.";
         }
-    }
-
-    if ($image && createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id)) {
-        $combattants = getAllCombattants($conn);
     } else {
-        echo "<p>Erreur lors de la création du combattant.</p>";
+        echo "Données du combattant non valides.";
     }
 }
 ?>
@@ -44,81 +52,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gérer les Combattants - Tableau de Bord Administratif</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <style>
-        body {
-            background-color: #000; /* Fond noir */
-        }
-        .card {
-            border-radius: 15px;
-            background: #212121;
-            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5),
-            -5px -5px 10px rgba(255, 255, 255, 0.1);
-            overflow: hidden;
-            transition: transform 0.2s;
-            margin-bottom: 20px;
-            width: 200px;
-            height: 350px;
-        }
-        .card:hover {
-            transform: scale(1.05);
-        }
-        .card img {
-            width: 100%;
-            height: 120px; /* Hauteur de l'image */
-            object-fit: cover;
-        }
-        .card-content {
-            padding: 10px;
-            color: #fff;
-        }
-        .btn {
-            display: inline-block;
-            padding: 5px 10px;
-            margin-top: 5px;
-            border-radius: 5px;
-            color: #fff;
-            background-color: #007bff;
-            text-decoration: none;
-            text-align: center;
-        }
-        .btn-danger {
-            background-color: #dc3545;
-        }
-        .grid-container {
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
-            gap: 20px;
-        }
-    </style>
-    <script>
-        function confirmDelete() {
-            return confirm("Êtes-vous sûr de vouloir supprimer ce combattant ?");
-        }
-    </script>
 </head>
-<body>
-<div id="sidebar">
-    <!-- Contenu de la barre latérale (si présent) -->
-</div>
-<div id="content" class="p-4">
-    <h1 class="text-3xl font-bold mb-6 text-white">Gérer les Combattants</h1>
-    <button id="addCombatantBtn" class="mb-4 p-2 bg-blue-600 text-white rounded hover:bg-blue-700">Ajouter un combattant</button>
+<body class="bg-gray-200">
+<div class="container mx-auto mt-10 p-5">
+    <h2 class="text-3xl font-semibold text-gray-800 mb-5">Gérer les Combattants</h2>
+    <button id="addCombatantBtn" class="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ajouter un combattant</button>
 
-    <div class="grid-container">
-        <?php foreach ($combattants as $combattant): ?>
-            <div class="card">
-                <img src="<?= htmlspecialchars($combattant['image']) ?: 'path/to/default-image.png' ?>" alt="Image de combattant">
-                <div class="card-content">
-                    <h2 class="text-lg font-semibold"><?= htmlspecialchars($combattant['nom']) ?></h2>
-                    <p><?= htmlspecialchars($combattant['prenom']) ?> "<?= htmlspecialchars($combattant['surnom']) ?>"</p>
-                    <p><?= htmlspecialchars($combattant['description']) ?></p>
-                    <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="btn">Éditer</a>
-                    <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="btn btn-danger" onclick="return confirmDelete()">Supprimer</a>
-                </div>
+    <div id="addCombatantForm" class="hidden">
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-4">
+                <label for="nom" class="block text-sm font-bold mb-2">Nom :</label>
+                <input type="text" id="nom" name="nom" required class="w-full p-2 border rounded">
             </div>
+            <div class="mb-4">
+                <label for="prenom" class="block text-sm font-bold mb-2">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" required class="w-full p-2 border rounded">
+            </div>
+            <div class="mb-4">
+                <label for="surnom" class="block text-sm font-bold mb-2">Surnom :</label>
+                <input type="text" id="surnom" name="surnom" class="w-full p-2 border rounded">
+            </div>
+            <div class="mb-4">
+                <label for="description" class="block text-sm font-bold mb-2">Description :</label>
+                <textarea id="description" name="description" required class="w-full p-2 border rounded"></textarea>
+            </div>
+            <div class="mb-4">
+                <label for="categorie_id" class="block text-sm font-bold mb-2">Catégorie :</label>
+                <select id="categorie_id" name="categorie_id" required class="w-full p-2 border rounded">
+                    <!-- Options de catégories ici -->
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="image" class="block text-sm font-bold mb-2">Image :</label>
+                <input type="file" id="image" name="image" required class="w-full p-2 border rounded">
+            </div>
+            <div class="flex justify-end">
+                <button type="submit" name="add_combattant" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Créer</button>
+            </div>
+        </form>
+    </div>
+
+    <!-- Affichage des combattants existants -->
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <?php foreach ($combattants as $combattant): ?>
+            <!-- Carte du combattant -->
         <?php endforeach; ?>
     </div>
 </div>
@@ -126,7 +105,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
 <script>
     document.getElementById('addCombatantBtn').addEventListener('click', function() {
         var form = document.getElementById('addCombatantForm');
-        form.style.display = form.style.display === 'none' ? 'block' : 'none';
+        form.classList.toggle('hidden');
     });
 </script>
 </body>
