7d6cfdfcea09dd27658a17d764ce0dff3d51128e
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 23:01:13 +0100

    ccc

--- admin-manage-combattants.php
+++ admin-manage-combattants.php
@@ -48,66 +48,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
     <title>Gérer les Combattants - Tableau de Bord Administratif</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-black text-white" style="padding-top: 5rem;"> <!-- Utilisation de style inline pour le padding-top -->
-
-<!-- Barre de navigation ici (si vous en avez une) -->
-
-<div id="content" class="p-4">
+<body class="bg-gray-900 text-gray-100" style="padding-top: 5rem;">
+<div id="content" class="px-4 md:px-10 py-4 md:py-7">
     <h1 class="text-3xl font-bold mb-6">Gérer les Combattants</h1>
-    <button id="addCombatantBtn" class="mb-4 px-4 py-2 bg-blue-600 rounded hover:bg-blue-700 transition duration-300">Ajouter un combattant</button>
-
-    <div id="addCombatantForm" class="hidden"> <!-- Utilisation de la classe hidden de Tailwind -->
-        <form action="admin-manage-combattants.php" method="post" enctype="multipart/form-data" class="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <div class="mb-4">
-                <input type="text" name="nom" placeholder="Nom" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow" required>
-            </div>
-            <div class="mb-4">
-                <input type="text" name="prenom" placeholder="Prénom" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow" required>
-            </div>
-            <div class="mb-4">
-                <input type="text" name="surnom" placeholder="Surnom" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow">
-            </div>
-            <div class="mb-4">
-                <textarea name="description" placeholder="Description" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow"></textarea>
-            </div>
-            <div class="mb-4">
-                <select name="categorie_id" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow" required>
-                    <?php
-                    // Chargez ici les catégories depuis la base de données
-                    $categories = getAllCategories($conn); // Assurez-vous que cette fonction fonctionne correctement
-                    foreach ($categories as $categorie) {
-                        echo "<option value=\"{$categorie['id']}\">{$categorie['nom']}</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="mb-4">
-                <input type="file" name="image" class="w-full px-3 py-2 text-gray-700 bg-white border rounded shadow">
-            </div>
-            <div>
-                <input type="submit" name="add_combattant" value="Ajouter Combattant" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition duration-300 cursor-pointer">
-            </div>
+    <button id="addCombatantBtn" class="mb-4 px-6 py-3 bg-blue-600 rounded-lg shadow-lg text-white font-semibold hover:bg-blue-700 transition duration-300">Ajouter un combattant</button>
+    <div id="addCombatantForm" class="hidden">
+        <!-- Formulaire pour ajouter un combattant -->
+        <form action="admin-manage-combattants.php" method="post" enctype="multipart/form-data" class="bg-gray-800 p-6 rounded-lg shadow-xl">
+            <input type="text" name="nom" placeholder="Nom" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow" required>
+            <input type="text" name="prenom" placeholder="Prénom" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow" required>
+            <input type="text" name="surnom" placeholder="Surnom" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow">
+            <textarea name="description" placeholder="Description" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow"></textarea>
+            <select name="categorie_id" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow" required>
+                <?php
+                $categories = getAllCategories($conn);
+                foreach ($categories as $categorie) {
+                    echo "<option value=\"{$categorie['id']}\">{$categorie['nom']}</option>";
+                }
+                ?>
+            </select>
+            <input type="file" name="image" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow">
+            <input type="submit" name="add_combattant" value="Ajouter Combattant" class="px-6 py-3 bg-green-500 text-white rounded-lg shadow-lg hover:bg-green-600 transition duration-300 cursor-pointer">
         </form>
     </div>
-
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         <?php foreach ($combattants as $combattant): ?>
-            <div class="card bg-gray-800 rounded overflow-hidden shadow-lg transform transition duration-500 hover:scale-105">
-                <img src="<?= htmlspecialchars($combattant['image']) ?: 'path/to/default-image.png' ?>" alt="Image de combattant" class="w-full h-32 object-cover">
-                <div class="card-content p-4">
-                    <h2 class="text-lg font-semibold"><?= htmlspecialchars($combattant['nom']) ?> <?= htmlspecialchars($combattant['prenom']) ?></h2>
+            <div class="bg-gray-800 rounded-xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105">
+                <img src="<?= htmlspecialchars($combattant['image']) ?: 'path/to/default-image.png' ?>" alt="Image de combattant" class="w-full h-40 object-cover">
+                <div class="p-5">
+                    <h2 class="text-2xl font-semibold"><?= htmlspecialchars($combattant['nom']) ?> <?= htmlspecialchars($combattant['prenom']) ?></h2>
                     <p class="text-gray-400"><?= htmlspecialchars($combattant['surnom']) ?></p>
                     <p class="text-sm mt-2"><?= htmlspecialchars($combattant['description']) ?></p>
                     <div class="flex justify-between items-center mt-4">
-                        <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="btn bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Éditer</a>
-                        <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="btn bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded" onclick="return confirmDelete()">Supprimer</a>
+                        <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300">Éditer</a>
+                        <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition duration-300" onclick="return confirmDelete()">Supprimer</a>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
 </div>
-
 <script>
     document.getElementById('addCombatantBtn').addEventListener('click', function() {
         var form = document.getElementById('addCombatantForm');
