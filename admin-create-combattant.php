6afe5e310dfc9122833b19b5e8fa13a1687b6aca
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 19:45:44 +0100

    normalement_ca_marhce

--- admin-create-combattant.php
+++ admin-create-combattant.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'config.php';
+include 'common.php';
 include 'admin-functions.php';
 //checkAdmin(); // Assurez-vous que seul un admin peut accéder à cette page
 
@@ -28,25 +28,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Ajouter un Combattant</title>
-    <!-- Inclure ici vos liens CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<div id="content">
-    <h1>Ajouter un nouveau combattant</h1>
-    <form action="admin-create-combattant.php" method="post" enctype="multipart/form-data">
-        <label for="nom">Nom:</label>
-        <input type="text" name="nom" required><br>
+<body class="bg-gray-100">
+<div class="container mx-auto px-4 py-8">
+    <h1 class="text-2xl font-bold text-gray-700 mb-6">Ajouter un nouveau combattant</h1>
+    <form action="admin-create-combattant.php" method="post" enctype="multipart/form-data" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="nom">Nom:</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="nom" required>
+        </div>
 
-        <label for="prenom">Prénom:</label>
-        <input type="text" name="prenom" required><br>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="prenom">Prénom:</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="prenom" required>
+        </div>
 
-        <label for="description">Description:</label>
-        <textarea name="description"></textarea><br>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description:</label>
+            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="description"></textarea>
+        </div>
 
-        <label for="image">Image:</label>
-        <input type="file" name="image"><br>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="image">Image:</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="file" name="image">
+        </div>
 
-        <input type="submit" value="Ajouter le combattant">
+        <div class="flex items-center justify-between">
+            <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Ajouter le combattant">
+        </div>
     </form>
 </div>
 </body>
