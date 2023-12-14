7c1cead8eda19a62cd7e9a04c7e227810a311e3e
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 19:17:02 +0100

    Revert "fix"
    This reverts commit b54eb77d039e06871da8180a6b577dbd30fd567b.

--- admin-functions.php
+++ admin-functions.php
@@ -4,106 +4,117 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'common.php'; // Assurez-vous que ce chemin est correct
+require_once 'common.php'; // Assurez-vous que ce fichier existe et qu'il initialise $conn avec votre connexion mysqli.
 
-// Vérifie si la catégorie existe
+// Assurez-vous que les noms des tables et des colonnes correspondent à ceux de votre base de données.
+
+// Ajouter un nouveau combattant à la base de données
+function createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
+    if (!categorieExists($conn, $categorie_id)) {
+        return "Catégorie non trouvée.";
+    }
+    $query = "INSERT INTO combattants (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id);
+    if (!$stmt->execute()) {
+        return "Erreur lors de l'exécution de la requête: " . $stmt->error;
+    }
+    $stmt->close();
+    return true;
+}
+
+// Récupérer tous les combattants de la base de données
+function getAllCombattants($conn) {
+    $query = "SELECT combattants.*, categories.name as categorie_name FROM combattants INNER JOIN categories ON combattants.categorie_id = categories.id";
+    $combattants = [];
+    if ($result = $conn->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $combattants[] = $row;
+        }
+    }
+    return $combattants;
+}
+
+// Vérifier si une catégorie existe
 function categorieExists($conn, $id) {
-    $stmt = $conn->prepare("SELECT id FROM categories WHERE id = ?");
+    $query = "SELECT id FROM categories WHERE id = ?";
+    $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->num_rows > 0;
 }
 
-// Création d'un combattant
-function createCombattant($conn, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
+// Récupérer le nom d'une catégorie
+function getCategoryName($conn, $id) {
+    $query = "SELECT name FROM categories WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->bind_result($name);
+    $stmt->fetch();
+    $stmt->close();
+    return $name ?: false;
+}
+
+// Mettre à jour les informations d'un combattant
+function updateCombattant($conn, $id, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
     if (!categorieExists($conn, $categorie_id)) {
         return "Catégorie non trouvée.";
     }
-    $stmt = $conn->prepare("INSERT INTO combattants (nom, prenom, surnom, description, image, categorie_id) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi", $nom, $prenom, $surnom, $description, $image, $categorie_id);
+    $query = "UPDATE combattants SET nom = ?, prenom = ?, surnom = ?, description = ?, image = ?, categorie_id = ? WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("sssssii", $nom, $prenom, $surnom, $description, $image, $categorie_id, $id);
+    if (!$stmt->execute()) {
+        return "Erreur lors de l'exécution de la requête: " . $stmt->error;
+    }
+    $stmt->close();
+    return true;
+}
+
+// Fonction pour supprimer un combattant
+function deleteCombattant($conn, $id) {
+    $query = "DELETE FROM combattants WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
     $stmt->execute();
-    return $stmt->insert_id;
+    $stmt->close();
 }
 
-// Obtenir tous les combattants
-function getAllCombattants($conn) {
-    $result = $conn->query("SELECT combattants.*, categories.name as categorie_name FROM combattants INNER JOIN categories ON combattants.categorie_id = categories.id");
-    return $result->fetch_all(MYSQLI_ASSOC);
-}
-
-// Obtenir un combattant spécifique
+// Fonction pour obtenir un combattant spécifique
 function getCombattant($conn, $id) {
-    $stmt = $conn->prepare("SELECT * FROM combattants WHERE id = ?");
+    $query = "SELECT * FROM combattants WHERE id = ?";
+    $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->fetch_assoc();
 }
 
-// Mettre à jour un combattant
-function updateCombattant($conn, $id, $nom, $prenom, $surnom, $description, $image, $categorie_id) {
-    if (!categorieExists($conn, $categorie_id)) {
-        return "Catégorie non trouvée.";
-    }
-    $stmt = $conn->prepare("UPDATE combattants SET nom = ?, prenom = ?, surnom = ?, description = ?, image = ?, categorie_id = ? WHERE id = ?");
-    $stmt->bind_param("sssssii", $nom, $prenom, $surnom, $description, $image, $categorie_id, $id);
-    $stmt->execute();
-    return $stmt->affected_rows;
-}
-
-// Supprimer un combattant
-function deleteCombattant($conn, $id) {
-    $stmt = $conn->prepare("DELETE FROM combattants WHERE id = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    return $stmt->affected_rows;
-}
-
-// Création d'une catégorie
-function createCategorie($conn, $nom) {
-    $stmt = $conn->prepare("INSERT INTO categories (name) VALUES (?)");
-    $stmt->bind_param("s", $nom);
-    $stmt->execute();
-    return $stmt->insert_id;
-}
-
-// Obtenir toutes les catégories
-function getAllCategories($conn) {
-    $result = $conn->query("SELECT * FROM categories");
-    return $result->fetch_all(MYSQLI_ASSOC);
-}
-
-// Supprimer une catégorie
-function deleteCategorie($conn, $id) {
-    $stmt = $conn->prepare("DELETE FROM categories WHERE id = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    return $stmt->affected_rows;
-}
-
-// Création d'un événement
+// Ajouter un événement
+// Assurez-vous que cette fonction est nécessaire ou mettez à jour la structure de la base de données en conséquence.
 function createEvenement($conn, $nom, $date, $lieu, $description) {
-    $stmt = $conn->prepare("INSERT INTO evenements (nom, date, lieu, description) VALUES (?, ?, ?, ?)");
+    $query = "INSERT INTO evenements (nom, date, lieu, description) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($query);
     $stmt->bind_param("ssss", $nom, $date, $lieu, $description);
-    $stmt->execute();
-    return $stmt->insert_id;
+    if (!$stmt->execute()) {
+        return "Erreur lors de l'exécution de la requête: " . $stmt->error;
+    }
+    $stmt->close();
+    return true;
 }
 
-// Obtenir tous les événements
+// Récupérer tous les événements
+// Assurez-vous que cette fonction est nécessaire ou mettez à jour la structure de la base de données en conséquence.
 function getAllEvenements($conn) {
-    $result = $conn->query("SELECT * FROM evenements ORDER BY date ASC");
-    return $result->fetch_all(MYSQLI_ASSOC);
+    $query = "SELECT * FROM evenements ORDER BY date ASC";
+    $evenements = [];
+    if ($result = $conn->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $evenements[] = $row;
+        }
+    }
+    return $evenements;
 }
-
-// Supprimer un événement
-function deleteEvenement($conn, $id) {
-    $stmt = $conn->prepare("DELETE FROM evenements WHERE id = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    return $stmt->affected_rows;
-}
-
-// Vous pouvez ajouter d'autres fonctions si nécessaire...
 
 ?>
