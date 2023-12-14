bc4a10a89571f2f3f4e65e01041b22dbb6ee269d
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-15 00:37:00 +0100

    ddd

--- admin-manage-events.php
+++ admin-manage-events.php
@@ -7,7 +7,7 @@ require_once 'admin-functions.php';
 require_once 'common.php';
 
 $evenements = getAllEvenements($conn);
-$combattants = getAllCombattants($conn); // Assurez-vous d'avoir cette fonction dans admin-functions.php
+$combattants = getAllCombattants($conn);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['add_event'])) {
@@ -19,21 +19,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $combattant1_id = $_POST['combattant1_id'];
         $combattant2_id = $_POST['combattant2_id'];
 
-        createEvenement($nom, $date, $heure, $lieu, $description, $combattant1_id, $combattant2_id, $conn);
-    } elseif (isset($_POST['edit_event'])) {
-        // Logique pour éditer un événement
-    } elseif (isset($_POST['delete_event'])) {
-        // Logique pour supprimer un événement
-    }
-}
+        // Gérer l'upload des images pour les combattants
+        // Cette fonction doit être définie pour traiter les fichiers d'upload et retourner le chemin de l'image ou une erreur
+        $image_combattant1 = handleImageUpload($_FILES['image_combattant1']);
+        $image_combattant2 = handleImageUpload($_FILES['image_combattant2']);
 
+        createEvenement($conn, $nom, $date, $heure, $lieu, $description, $combattant1_id, $combattant2_id, $image_combattant1, $image_combattant2);
+    }
+    // Les sections pour 'edit_event' et 'delete_event' doivent être complétées de manière similaire
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gérer les Événements</title>
+    <link rel="stylesheet" href="/style/admin-dashboard.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-900 text-gray-100">
@@ -41,26 +44,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <div id="content" class="px-4 md:px-10 py-4 md:py-7">
     <h1 class="text-3xl font-bold mb-6">Gérer les Événements</h1>
-    <button id="addEventBtn">Ajouter un événement</button>
+    <button id="addEventBtn" class="mb-4">Ajouter un événement</button>
 
     <!-- Formulaire pour ajouter un événement -->
     <div id="addEventForm" class="hidden">
         <form action="admin-manage-event.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="nom" placeholder="Nom de l'événement" required>
-            <input type="date" name="date" required>
-            <input type="time" name="heure" required>
-            <input type="text" name="lieu" placeholder="Lieu" required>
-            <textarea name="description" placeholder="Description"></textarea>
+            <label for="nom">Nom de l'événement:</label>
+            <input type="text" name="nom" required><br>
+
+            <label for="date">Date:</label>
+            <input type="date" name="date" required><br>
+
+            <label for="heure">Heure:</label>
+            <input type="time" name="heure" required><br>
+
+            <label for="lieu">Lieu:</label>
+            <input type="text" name="lieu" required><br>
+
+            <label for="description">Description:</label>
+            <textarea name="description" required></textarea><br>
+
+            <label for="combattant1_id">Combattant 1:</label>
             <select name="combattant1_id" required>
                 <?php foreach ($combattants as $combattant): ?>
                     <option value="<?= $combattant['id'] ?>"><?= $combattant['nom'] ?></option>
                 <?php endforeach; ?>
-            </select>
+            </select><br>
+
+            <label for="combattant2_id">Combattant 2:</label>
             <select name="combattant2_id" required>
                 <?php foreach ($combattants as $combattant): ?>
                     <option value="<?= $combattant['id'] ?>"><?= $combattant['nom'] ?></option>
                 <?php endforeach; ?>
-            </select>
+            </select><br>
+
+            <label for="image_combattant1">Image du Combattant 1:</label>
+            <input type="file" name="image_combattant1" accept="image/*" required><br>
+
+            <label for="image_combattant2">Image du Combattant 2:</label>
+            <input type="file" name="image_combattant2" accept="image/*" required><br>
+
             <button type="submit" name="add_event">Ajouter</button>
         </form>
     </div>
@@ -69,12 +92,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         <?php foreach ($evenements as $evenement): ?>
             <div class="bg-gray-800 rounded-xl shadow-lg overflow-hidden">
-                <div class="p-5">
-                    <h2 class="text-2xl font-semibold"><?= htmlspecialchars($evenement['nom']) ?></h2>
-                    <p><?= htmlspecialchars($evenement['description']) ?></p>
-                    <img src="<?= getCombattantImage($evenement['combattant1_id'], $conn) ?>" alt="Combattant 1">
-                    <img src="<?= getCombattantImage($evenement['combattant2_id'], $conn) ?>" alt="Combattant 2">
-                    <!-- Boutons d'édition et de suppression -->
+                <div class="p-4">
+                    <h2 class="text-xl font-bold"><?= $evenement['nom'] ?></h2>
+                    <p class="text-gray-500">Date: <?= $evenement['date'] ?>, Heure: <?= $evenement['heure'] ?></p>
+                    <p class="mt-2"><?= $evenement['description'] ?></p>
+                </div>
+                <div class="flex justify-between p-4">
+                    <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold px-4 py-2 rounded">Éditer</button>
+                    <button class="bg-red-500 hover:bg-red-600 text-white font-bold px-4 py-2 rounded">Supprimer</button>
                 </div>
             </div>
         <?php endforeach; ?>
