a4d9868ff71ad08b9dc29ccf9f3705e60b2c226a
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 23:06:24 +0100

    dddddddd

--- admin-manage-combattants.php
+++ admin-manage-combattants.php
@@ -49,10 +49,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-900 text-gray-100" style="padding-top: 5rem;">
-<div id="content" class="px-4 md:px-10 py-4 md:py-7">
+<!-- Barre de navigation ici -->
+<?php include_once 'common.php';?>
+
+<div id="content" class="p-4">
     <h1 class="text-3xl font-bold mb-6">Gérer les Combattants</h1>
-    <button id="addCombatantBtn" class="mb-4 px-6 py-3 bg-blue-600 rounded-lg shadow-lg text-white font-semibold hover:bg-blue-700 transition duration-300">Ajouter un combattant</button>
-    <div id="addCombatantForm" class="hidden">
+    <button id="addCombatantBtn" class="mb-4 px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-300 focus:outline-none focus:ring focus:ring-blue-300">Ajouter un combattant</button>
+
         <!-- Formulaire pour ajouter un combattant -->
         <form action="admin-manage-combattants.php" method="post" enctype="multipart/form-data" class="bg-gray-800 p-6 rounded-lg shadow-xl">
             <input type="text" name="nom" placeholder="Nom" class="w-full px-3 py-2 mb-4 text-gray-700 bg-white border rounded shadow" required>
@@ -71,23 +74,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_combattant'])) {
             <input type="submit" name="add_combattant" value="Ajouter Combattant" class="px-6 py-3 bg-green-500 text-white rounded-lg shadow-lg hover:bg-green-600 transition duration-300 cursor-pointer">
         </form>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <?php foreach ($combattants as $combattant): ?>
-            <div class="bg-gray-800 rounded-xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105">
-                <img src="<?= htmlspecialchars($combattant['image']) ?: 'path/to/default-image.png' ?>" alt="Image de combattant" class="w-full h-40 object-cover">
-                <div class="p-5">
-                    <h2 class="text-2xl font-semibold"><?= htmlspecialchars($combattant['nom']) ?> <?= htmlspecialchars($combattant['prenom']) ?></h2>
+            <div class="card bg-gray-800 rounded-xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+                <img src="<?= htmlspecialchars($combattant['image']) ?: 'path/to/default-image.png' ?>" alt="Image de combattant" class="w-full h-32 object-cover">
+                <div class="card-content p-4">
+                    <h2 class="text-xl font-semibold"><?= htmlspecialchars($combattant['nom']) ?> <?= htmlspecialchars($combattant['prenom']) ?></h2>
                     <p class="text-gray-400"><?= htmlspecialchars($combattant['surnom']) ?></p>
                     <p class="text-sm mt-2"><?= htmlspecialchars($combattant['description']) ?></p>
                     <div class="flex justify-between items-center mt-4">
-                        <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300">Éditer</a>
-                        <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition duration-300" onclick="return confirmDelete()">Supprimer</a>
+                        <a href="edit-combattant.php?id=<?= $combattant['id'] ?>" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-lg shadow focus:outline-none focus:ring focus:ring-blue-300">Éditer</a>
+                        <a href="delete-combattant.php?id=<?= $combattant['id'] ?>" class="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-lg shadow focus:outline-none focus:ring focus:ring-red-300" onclick="return confirmDelete()">Supprimer</a>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
 </div>
+
 <script>
     document.getElementById('addCombatantBtn').addEventListener('click', function() {
         var form = document.getElementById('addCombatantForm');
