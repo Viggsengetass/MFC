cd39bfd0f4d20abf98ccdc95c70350f957f6aa6e
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-14 20:52:26 +0100

    ggggggggggggggggg

--- admin-create-combattant.php
+++ admin-create-combattant.php
@@ -1,41 +1,35 @@
 <?php
 session_start();
-
-// Supposons que common.php contienne la fonction pour obtenir une connexion à la base de données
-require_once 'common.php';
-require_once 'admin-functions.php'; // Ce fichier doit contenir les fonctions spécifiques à l'administration, telles que checkAdmin
+require_once 'common.php'; // Ce fichier doit inclure la connexion à la base de données et des fonctions communes
+require_once 'admin-functions.php'; // Ce fichier doit inclure des fonctions spécifiques à l'administration comme checkAdmin
 
 // Fonction pour valider les données du combattant
 function validateCombatant($nom, $prenom, $description, $image) {
     if (empty($nom) || empty($prenom) || empty($description)) {
-        return "Le nom, le prénom et la description sont obligatoires.";
+        return "Le nom, le prénom et la description ne peuvent pas être vides.";
     }
     if (empty($image)) {
-        return "Une image doit être sélectionnée.";
+        return "Vous devez sélectionner une image.";
     }
-    // Ajoutez d'autres règles de validation si nécessaire
+    // Effectuez ici d'autres validations au besoin
     return true;
 }
 
 // Fonction pour créer un combattant dans la base de données
-function createCombattant($conn, $nom, $prenom, $description, $image) {
-    $targetDir = "uploads/"; // Ce répertoire doit être présent et accessible en écriture sur le serveur
-    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-        $stmt = $conn->prepare("INSERT INTO combattants (nom, prenom, description, image) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $nom, $prenom, $description, $targetFile);
-        $result = $stmt->execute();
-        $stmt->close();
-        return $result;
-    } else {
-        return "Erreur lors du téléchargement de l'image.";
-    }
+function createCombattant($conn, $nom, $prenom, $description, $imagePath) {
+    $stmt = $conn->prepare("INSERT INTO combattants (nom, prenom, description, image) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $nom, $prenom, $description, $imagePath);
+    $success = $stmt->execute();
+    $stmt->close();
+    return $success;
 }
 
 $error_message = '';
+$success_message = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $conn = getDatabaseConnection(); // Assurez-vous que cette fonction retourne une connexion valide à la base de données
+    $conn = getDatabaseConnection(); // Assurez-vous que cette fonction existe et retourne une connexion valide à la base de données
+
     $nom = $_POST['nom'] ?? '';
     $prenom = $_POST['prenom'] ?? '';
     $description = $_POST['description'] ?? '';
@@ -43,12 +37,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $validationResult = validateCombatant($nom, $prenom, $description, $image);
     if ($validationResult === true) {
-        $creationResult = createCombattant($conn, $nom, $prenom, $description, $image);
-        if ($creationResult === true) {
-            header('Location: admin-manage-combattants.php');
-            exit();
+        $targetDir = "uploads/";
+        $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            if (createCombattant($conn, $nom, $prenom, $description, $targetFile)) {
+                $success_message = "Combattant ajouté avec succès.";
+            } else {
+                $error_message = "Erreur lors de l'ajout dans la base de données.";
+            }
         } else {
-            $error_message = $creationResult; // Si createCombattant retourne un message d'erreur
+            $error_message = "Erreur lors du téléchargement de l'image.";
         }
     } else {
         $error_message = $validationResult;
@@ -63,33 +61,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
-<div class="container mx-auto px-4 py-8">
-    <h1 class="text-2xl font-bold text-gray-700 mb-6">Ajouter un nouveau combattant</h1>
+<div class="container mx-auto mt-10 p-5">
+    <h2 class="text-xl font-bold mb-5">Ajouter un nouveau combattant</h2>
     <?php if ($error_message): ?>
-        <div class="mb-4 text-red-500">
-            <?= htmlspecialchars($error_message) ?>
-        </div>
+        <p class="text-red-500"><?= htmlspecialchars($error_message) ?></p>
+    <?php elseif ($success_message): ?>
+        <p class="text-green-500"><?= htmlspecialchars($success_message) ?></p>
     <?php endif; ?>
-    <form action="admin-create-combattant.php" method="post" enctype="multipart/form-data" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="nom">Nom :</label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="nom" required>
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="prenom">Prénom :</label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="prenom" required>
+            <label for="nom" class="block text-sm font-bold mb-2">Nom:</label>
+            <input type="text" id="nom" name="nom" required class="w-full p-2 border border-gray-300 rounded">
         </div>
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description :</label>
-            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="description"></textarea>
+            <label for="prenom" class="block text-sm font-bold mb-2">Prénom:</label>
+            <input type="text" id="prenom" name="prenom" required class="w-full p-2 border border-gray-300 rounded">
         </div>
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="image">Image :</label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="file" name="image" required>
+            <label for="description" class="block text-sm font-bold mb-2">Description:</label>
+            <textarea id="description" name="description" required class="w-full p-2 border border-gray-300 rounded"></textarea>
         </div>
-        <div class="flex items-center justify-between">
-            <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Ajouter le combattant">
+        <div class="mb-4">
+            <label for="image" class="block text-sm font-bold mb-2">Image:</label>
+            <input type="file" id="image" name="image" required class="w-full p-2 border border-gray-300 rounded">
         </div>
+        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Ajouter le combattant</button>
     </form>
 </div>
 </body>
