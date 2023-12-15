aa5a747fb5196a38d4eacfa0778dbafd6feffe72
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-15 04:03:36 +0100

    ccccc

--- login.php
+++ login.php
@@ -19,6 +19,7 @@ session_start();
 <body class="bg-gray-100">
 <div class="container">
     <?php
+    session_start();
     if (isset($_SESSION['user'])) {
         echo '<p>Bienvenue, ' . $_SESSION['user']['username'] . '!</p>';
         echo '<p>Vous êtes connecté en tant que ' . $_SESSION['user']['role'] . '.</p>';
@@ -26,13 +27,7 @@ session_start();
     } else {
         ?>
         <input type="checkbox" id="login_toggle">
-        <form class="form" method="post" action="login-process.php" class="space-y-4">
-            <?php
-            if (isset($_SESSION['login_error'])) {
-                echo '<p class="error">' . $_SESSION['login_error'] . '</p>';
-                unset($_SESSION['login_error']);
-            }
-            ?>
+        <form class="form" method="post" action="login-process.php">
             <div class="form_front">
                 <div class="form_details">Login</div>
                 <input placeholder="Username" name="username" class="input" type="text">
@@ -47,6 +42,27 @@ session_start();
     }
     ?>
 </div>
+<script>
+    window.onload = function() {
+        <?php if (isset($_SESSION['message'])) { ?>
+        var msgDiv = document.createElement("div");
+        msgDiv.classList.add("popup", "success");
+        msgDiv.textContent = "<?php echo $_SESSION['message']; ?>";
+        document.body.appendChild(msgDiv);
+        setTimeout(function() { msgDiv.remove(); }, 3000);
+        <?php unset($_SESSION['message']); ?>
+        <?php } ?>
+
+        <?php if (isset($_SESSION['error'])) { ?>
+        var errorDiv = document.createElement("div");
+        errorDiv.classList.add("popup", "error");
+        errorDiv.textContent = "<?php echo $_SESSION['error']; ?>";
+        document.body.appendChild(errorDiv);
+        setTimeout(function() { errorDiv.remove(); }, 3000);
+        <?php unset($_SESSION['error']); ?>
+        <?php } ?>
+    };
+</script>
 </body>
 
 </html>
