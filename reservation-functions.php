2537d51a395a86f15189657e3f657278af483c21
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-15 10:43:49 +0100

    DDDDDDDDDD

--- reservation-functions.php
+++ reservation-functions.php
@@ -12,49 +12,85 @@ require_once 'common.php';
 require_once 'reservation-functions.php';
 require_once 'admin-functions.php';
 
-// Fonction pour envoyer un e-mail de confirmation
-function sendConfirmationEmail($destinataire, $evenement, $nombre_billets) {
-    $sujet = "Confirmation de Réservation pour l'Événement " . htmlspecialchars($evenement['nom']);
-    $message = "Vous avez réservé avec succès pour l'événement " . htmlspecialchars($evenement['nom']) . ".\n";
-    $message .= "Date: " . htmlspecialchars($evenement['date']) . "\n";
-    $message .= "Heure: " . htmlspecialchars($evenement['heure']) . "\n";
-    $message .= "Lieu: " . htmlspecialchars($evenement['lieu']) . "\n";
-    $message .= "Nombre de Billets: " . htmlspecialchars($nombre_billets) . "\n";
+// Inclusion de la bibliothèque PHPMailer (assurez-vous qu'elle est correctement installée)
+require_once 'PHPMailer/PHPMailer.php';
+require_once 'PHPMailer/SMTP.php';
 
-    return mail($destinataire, $sujet, $message);
-}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+// Récupération de la liste des événements
+$evenements = getAllEvenements($conn);
+
+// Récupération de l'identifiant de l'utilisateur depuis la session
+$utilisateur_id = $_SESSION['user']['id'] ?? null; // Utilisez la même clé 'user' que dans d'autres parties de votre application
 
 // Vérification de la connexion de l'utilisateur
-if (!isset($_SESSION['user']['id'])) {
+if (!$utilisateur_id) {
     die("Vous devez être connecté pour faire une réservation.");
+}
+
+// Fonction pour envoyer un e-mail de confirmation
+function envoyerEmailConfirmation($evenementNom, $nombreBillets, $adresseEmail)
+{
+    // Création de l'objet PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Configuration du serveur SMTP (vous devrez le configurer correctement)
+        $mail->isSMTP();
+        $mail->Host = 'smtp.example.com'; // Remplacez par votre serveur SMTP
+        $mail->SMTPAuth = true;
+        $mail->Username = 'your_username'; // Remplacez par votre nom d'utilisateur SMTP
+        $mail->Password = 'your_password'; // Remplacez par votre mot de passe SMTP
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Utilisez STARTTLS
+        $mail->Port = 587;
+
+        // Configuration de l'émetteur et du destinataire
+        $mail->setFrom('paulito.antoine@gmail.com', 'Paul ANTOINE');
+        $mail->addAddress($adresseEmail);
+
+        // Sujet et corps du message
+        $mail->isHTML(true);
+        $mail->Subject = 'Confirmation de réservation';
+        $mail->Body = "Vous avez réservé $nombreBillets billet(s) pour l'événement : $evenementNom.";
+
+        // Envoi de l'e-mail
+        $mail->send();
+
+        return true;
+    } catch (Exception $e) {
+        return "Erreur lors de l'envoi de l'e-mail : {$mail->ErrorInfo}";
+    }
 }
 
 // Traitement du formulaire de réservation
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $evenement_id = $_POST['evenement_id'];
     $nombre_billets = $_POST['nombre_billets'];
-    $result = ajouterReservation($conn, $_SESSION['user']['id'], $evenement_id, $nombre_billets);
-    if ($result === true) {
-        // Récupération des informations sur l'événement
-        $evenement = getEvenement($conn, $evenement_id);
+    $result = ajouterReservation($conn, $utilisateur_id, $evenement_id, $nombre_billets);
 
-        if ($evenement) {
-            // Envoyer l'e-mail de confirmation
-            $destinataire = $_SESSION['user']['email']; // Adresse e-mail de l'utilisateur connecté
-            if (sendConfirmationEmail($destinataire, $evenement, $nombre_billets)) {
-                // Redirigez l'utilisateur vers une page de confirmation de réservation
-                header('Location: confirmation.php');
-                exit();
-            } else {
-                // Gestion de l'erreur d'envoi de l'e-mail
-                echo "L'envoi de l'e-mail de confirmation a échoué.";
-            }
+    if ($result === true) {
+        // Récupération du nom de l'événement pour l'e-mail de confirmation
+        $evenement = getEvenementById($conn, $evenement_id);
+        $evenementNom = $evenement['nom'];
+
+        // Envoi de l'e-mail de confirmation
+        $adresseEmail = $_SESSION['user']['email']; // Adresse e-mail de l'utilisateur connecté
+        $emailResult = envoyerEmailConfirmation($evenementNom, $nombre_billets, $adresseEmail);
+
+        if ($emailResult === true) {
+            // Redirection vers le panier avec un message de succès
+            $_SESSION['message'] = "Réservation ajoutée avec succès au panier. Un e-mail de confirmation a été envoyé.";
+            header('Location: panier.php');
+            exit();
         } else {
-            echo "Événement non trouvé.";
+            // Afficher un message d'erreur si l'envoi de l'e-mail échoue
+            $erreur_message = "Erreur lors de la réservation : $result. Erreur d'envoi d'e-mail : $emailResult";
         }
     } else {
         // Afficher un message d'erreur si la réservation échoue
-        $erreur_message = "Erreur lors de la réservation : " . $result;
+        $erreur_message = "Erreur lors de la réservation : $result";
     }
 }
 ?>
