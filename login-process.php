a5cd83def1d8044204cfbc293f2a1b656981b2ec
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-12-15 06:50:00 +0100

    ssss

--- login-process.php
+++ login-process.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-include 'config.php'; // Inclure votre fichier de configuration de base de données
+include 'common.php'; // Inclure votre fichier de configuration de base de données
 
 // Fonction pour la connexion de l'utilisateur
 function loginUser($conn, $username, $password) {
